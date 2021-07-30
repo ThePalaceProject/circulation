7143a781c4e71e1f2f2cc3164193cb6c9486dd5d
Jonathan Green <jonathan@razzard.com>
2021-07-30 10:30:53 -0300

    Update tests

--- tests/admin/test_config.py
+++ tests/admin/test_config.py
@@ -3,8 +3,8 @@ from typing import Optional
 
 import pytest
 
-from admin.config import Configuration as AdminConfig
-from admin.config import OperationalMode
+from api.admin.config import Configuration as AdminConfig
+from api.admin.config import OperationalMode
 
 
 class TestAdminUI(object):
@@ -20,7 +20,7 @@ class TestAdminUI(object):
         'package_name, package_version, mode, expected_result_startswith',
         [
             [None, None, OperationalMode.production,
-             'https://cdn.jsdelivr.net/npm/@thepalaceproject/library-registry-admin@'],
+             'https://cdn.jsdelivr.net/npm/@thepalaceproject/circulation-admin@'],
             ['@some-scope/some-package', '1.0.0', OperationalMode.production,
              'https://cdn.jsdelivr.net/npm/@some-scope/some-package@1.0.0'],
             ['some-package', '1.0.0', OperationalMode.production,
@@ -31,41 +31,41 @@ class TestAdminUI(object):
         ])
     def test_package_url(self, package_name: Optional[str], package_version: Optional[str],
                          mode: OperationalMode, expected_result_startswith: str):
-        self._set_env('TPP_LIBRARY_REGISTRY_ADMIN_PACKAGE_NAME', package_name)
-        self._set_env('TPP_LIBRARY_REGISTRY_ADMIN_PACKAGE_VERSION', package_version)
+        self._set_env('TPP_CIRCULATION_ADMIN_PACKAGE_NAME', package_name)
+        self._set_env('TPP_CIRCULATION_ADMIN_PACKAGE_VERSION', package_version)
         result = AdminConfig.package_url(_operational_mode=mode)
         assert result.startswith(expected_result_startswith)
 
     @pytest.mark.parametrize(
         'package_name, package_version, expected_result',
         [
-            [None, None, '/my-base-dir/node_modules/@thepalaceproject/library-registry-admin'],
-            [None, '1.0.0', '/my-base-dir/node_modules/@thepalaceproject/library-registry-admin'],
+            [None, None, '/my-base-dir/node_modules/@thepalaceproject/circulation-admin'],
+            [None, '1.0.0', '/my-base-dir/node_modules/@thepalaceproject/circulation-admin'],
             ['some-package', '1.0.0', '/my-base-dir/node_modules/some-package'],
         ])
     def test_package_development_directory(self, package_name: Optional[str], package_version: Optional[str],
                                            expected_result: str):
-        self._set_env('TPP_LIBRARY_REGISTRY_ADMIN_PACKAGE_NAME', package_name)
-        self._set_env('TPP_LIBRARY_REGISTRY_ADMIN_PACKAGE_VERSION', package_version)
+        self._set_env('TPP_CIRCULATION_ADMIN_PACKAGE_NAME', package_name)
+        self._set_env('TPP_CIRCULATION_ADMIN_PACKAGE_VERSION', package_version)
         result = AdminConfig.package_development_directory(_base_dir='/my-base-dir')
         assert result == expected_result
 
     @pytest.mark.parametrize(
         'asset_key, operational_mode, expected_result',
         [
-            ['admin_css', OperationalMode.development, '/static/library-registry-admin.css'],
+            ['admin_css', OperationalMode.development, '/static/circulation-admin.css'],
             ['admin_css', OperationalMode.production,
-             'https://cdn.jsdelivr.net/npm/known-package-name@1.0.0/dist/library-registry-admin.css'],
-            ['admin_js', OperationalMode.development, '/static/library-registry-admin.js'],
+             'https://cdn.jsdelivr.net/npm/known-package-name@1.0.0/dist/circulation-admin.css'],
+            ['admin_js', OperationalMode.development, '/static/circulation-admin.js'],
             ['admin_js', OperationalMode.production,
-             'https://cdn.jsdelivr.net/npm/known-package-name@1.0.0/dist/library-registry-admin.js'],
+             'https://cdn.jsdelivr.net/npm/known-package-name@1.0.0/dist/circulation-admin.js'],
             ['another-asset.jpg', OperationalMode.development, '/static/another-asset.jpg'],
             ['another-asset.jpg', OperationalMode.production,
              'https://cdn.jsdelivr.net/npm/known-package-name@1.0.0/dist/another-asset.jpg'],
         ]
     )
     def test_lookup_asset_url(self, asset_key: str, operational_mode: OperationalMode, expected_result: str):
-        self._set_env('TPP_LIBRARY_REGISTRY_ADMIN_PACKAGE_NAME', 'known-package-name')
-        self._set_env('TPP_LIBRARY_REGISTRY_ADMIN_PACKAGE_VERSION', '1.0.0')
+        self._set_env('TPP_CIRCULATION_ADMIN_PACKAGE_NAME', 'known-package-name')
+        self._set_env('TPP_CIRCULATION_ADMIN_PACKAGE_VERSION', '1.0.0')
         result = AdminConfig.lookup_asset_url(key=asset_key, _operational_mode=operational_mode)
         assert result == expected_result
