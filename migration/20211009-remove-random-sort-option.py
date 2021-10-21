2357ecc239abc2e0c4746e8913b35281032884e7
Viacheslav Bessonov <viacheslav.bessonov@hilbertteam.com>
2021-10-22 00:11:57 +0500

    Fix the import order in the mgiration

--- migration/20211009-remove-random-sort-option.py
+++ migration/20211009-remove-random-sort-option.py
@@ -5,13 +5,13 @@ import os
 import sys
 
 from contextlib2 import closing
-from migartion_scripts import RandomSortOptionRemover
 
 bin_dir = os.path.split(__file__)[0]
 package_dir = os.path.join(bin_dir, "..")
 sys.path.append(os.path.abspath(package_dir))
 
 from core.model import production_session
+from migartion_scripts import RandomSortOptionRemover
 
 with closing(production_session()) as db:
     remover = RandomSortOptionRemover()
