ccd5769bee709d975b0f51689e885f4b4e310d41
Tim DiLauro <tim.dilauro@gmail.com>
2021-12-03 09:44:01 -0500

    Add a litle documentation.

--- api/adobe_vendor_id.py
+++ api/adobe_vendor_id.py
@@ -696,8 +696,12 @@ class AuthdataUtility(object):
     OTHER_LIBRARIES_KEY = "other_libraries"
 
     @classmethod
-    def from_config(cls, library, _db=None):
+    def from_config(cls, library: Library, _db=None):
         """Initialize an AuthdataUtility from site configuration.
+
+        The library must be successfully registered with a discovery
+        integration in order for that integration to be a candidate
+        to provide configuration for the AuthdataUtility.
 
         :return: An AuthdataUtility if one is configured; otherwise None.
 
@@ -723,7 +727,6 @@ class AuthdataUtility(object):
             )
         )
 
-        integration = None
         for possible_integration in integrations:
             vendor_id = ConfigurationSetting.for_externalintegration(
                 cls.VENDOR_ID_KEY, possible_integration
@@ -734,13 +737,12 @@ class AuthdataUtility(object):
             if vendor_id and registration_status == RegistrationConstants.SUCCESS_STATUS:
                 integration = possible_integration
                 break
+        else:
+            return None
 
         library_uri = ConfigurationSetting.for_library(
             Configuration.WEBSITE_URL, library
         ).value
-
-        if not integration:
-            return None
 
         vendor_id = integration.setting(cls.VENDOR_ID_KEY).value
         library_short_name = ConfigurationSetting.for_library_and_externalintegration(
