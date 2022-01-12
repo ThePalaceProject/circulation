668731bf1a0b6b574ddd94ea767bd7cee6d4917c
Aseef Ahmed <aahmed@linz.govt.nz>
2022-01-12 21:22:43 +1300

    update docker file build

--- docker/simplified_app.sh
+++ docker/simplified_app.sh
@@ -42,7 +42,7 @@ useradd -ms /bin/bash -U simplified
 # Get the proper version of the codebase.
 # mkdir /var/www && cd /var/www
 # git clone https://github.com/${repo}.git circulation
-# chown simplified:simplified /var/www/circulation
+chown simplified:simplified /var/www/circulation
 cd /var/www/circulation
 git checkout $version
 
