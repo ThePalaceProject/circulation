f5b10d16998f9e746ab7e447e9ac9444571a87ca
Mark Raynsford <code@io7m.com>
2022-01-20 13:13:24 +0000

    :Start upgrading to SQLAlchemy 1.4.29
    This sets the version of SQLAlchemy to 1.4.29. Currently the following
    tests fail:
    
    ```
    py39-api-docker.tests.api.test_controller.TestBaseController
    py39-api-docker.tests.api.admin.controller.test_work_editor.TestWorkController
    py39-core-docker.tests.core.test_lane.TestDatabaseBackedFacets
    py39-core-docker.tests.core.test_lane.TestDatabaseBackedWorkList
    py39-core-docker.tests.core.test_lane.TestDatabaseBackedWorkList
    py39-core-docker.tests.core.test_metadata.TestMetadataImporter
    py39-core-docker.tests.core.test_metadata.TestMetadataImporter
    py39-core-docker.tests.core.test_scripts.TestReclassifyWorksForUncheckedSubjectsScript
    py39-core-docker.tests.core.models.test_collection.TestCollection
    py39-core-docker.tests.core.models.test_work.TestWork
    py39-core-docker.tests.core.models.test_work.TestWorkConsolidation
    ```
    
    The core/util/__init.py__'s fast_query_count() method has been updated to
    remove a six year old performance hack which appears to be incorrect in
    the 1.4 SQLAlchemy branch. This method is only used in one place in the
    code outside of the test suite, but there may be performance implications
    there as the method is no longer "fast"; it's just a standard SELECT count()
    query now.

--- core/util/__init__.py
+++ core/util/__init__.py
@@ -10,6 +10,7 @@ import sqlalchemy
 from money import Money
 from sqlalchemy import distinct
 from sqlalchemy.sql.functions import func
+from sqlalchemy import select
 
 # For backwards compatibility, import items that were moved to
 # languages.py
@@ -25,26 +26,8 @@ def batch(iterable, size=1):
 
 
 def fast_query_count(query):
-    """Counts the results of a query without using super-slow subquery"""
-
-    statement = query.enable_eagerloads(False).with_labels().statement
-    distinct_columns = statement._distinct
-    new_columns = [func.count()]
-    if isinstance(distinct_columns, list):
-        # When using distinct to select from the db, the distinct
-        # columns need to be incorporated into the count itself.
-        new_columns = [func.count(distinct(func.concat(*distinct_columns)))]
-
-        # Then we can remove the distinct criteria from the statement
-        # itself by setting it to its default value, False.
-        statement._distinct = False
-    count_q = statement.with_only_columns(new_columns).order_by(None)
-    count = query.session.execute(count_q).scalar()
-
-    if query._limit and query._limit < count:
-        return query._limit
-
-    return count
+    new_selection = select(func.count()).select_from(query.subquery())
+    return query.session.execute(new_selection).scalar_one()
 
 
 def slugify(text, length_limit=None):
