761f7aba5df40c2d42f6805b9734b539d4a9d3c0
Jonathan Green <jonathan@razzard.com>
2022-01-19 22:23:47 -0400

    Small updates

--- docker/simplified_app.sh
+++ docker/simplified_app.sh
@@ -6,23 +6,19 @@ set -x
 # Add packages we need to build the app and its dependencies
 apt-get update
 $minimal_apt_get_install --no-upgrade \
-  software-properties-common \
-  python3.6 \
+  python3 \
   python3-dev \
   python3-setuptools \
   python3-venv \
   python3-pip \
   gcc \
-  git \
-  libpcre3 \
   libpcre3-dev \
   libffi-dev \
   libjpeg-dev \
   libssl-dev \
   libpq-dev \
   libxmlsec1-dev \
-  libxmlsec1-openssl \
-  libxml2-dev
+  libxmlsec1-openssl
 
 # We should be able to drop these lines when we move to Python > 3.6
 # https://click.palletsprojects.com/en/5.x/python3/#python-3-surrogate-handling
@@ -58,7 +54,7 @@ poetry install --no-dev --no-root -E pg
 poetry cache clear -n --all pypi
 
 # Install NLTK.
-python3 -m textblob.download_corpora
+python3 -m textblob.download_corpora lite
 mv /root/nltk_data /usr/lib/
 
 # Link the repository code to /home/simplified and change permissions
