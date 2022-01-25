393762390422999b517594cff70f796920bb317d
Aseef Ahmed <aahmed@linz.govt.nz>
2022-01-26 07:33:52 +1300

    Update layer caching

--- docker/simplified_app.sh
+++ docker/simplified_app.sh
@@ -4,22 +4,6 @@ source /bd_build/buildconfig
 set -x
 
 # Add packages we need to build the app and its dependencies
-apt-get update
-$minimal_apt_get_install --no-upgrade \
-  python3 \
-  python3-dev \
-  python3-setuptools \
-  python3-venv \
-  python3-pip \
-  gcc \
-  libpcre3-dev \
-  libffi-dev \
-  libjpeg-dev \
-  libssl-dev \
-  libpq-dev \
-  libxmlsec1-dev \
-  libxmlsec1-openssl \
-  pkg-config
 
 # We should be able to drop these lines when we move to Python > 3.6
 # https://click.palletsprojects.com/en/5.x/python3/#python-3-surrogate-handling
