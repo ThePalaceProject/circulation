1165187b71fa4b9e5a69386252d3de9e7df66e2e
Aseef Ahmed <aahmed@linz.govt.nz>
2022-01-27 02:13:02 +1300

    Update Dockerfile

--- docker/simplified_app.sh
+++ docker/simplified_app.sh
@@ -4,11 +4,22 @@ source /bd_build/buildconfig
 set -x
 
 # Add packages we need to build the app and its dependencies
-
-# We should be able to drop these lines when we move to Python > 3.6
-# https://click.palletsprojects.com/en/5.x/python3/#python-3-surrogate-handling
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+apt-get update
+$minimal_apt_get_install --no-upgrade \
+  python3 \
+  python3-dev \
+  python3-setuptools \
+  python3-venv \
+  python3-pip \
+  gcc \
+  libpcre3-dev \
+  libffi-dev \
+  libjpeg-dev \
+  libssl-dev \
+  libpq-dev \
+  libxmlsec1-dev \
+  libxmlsec1-openssl \
+  pkg-config
 
 # Create a user.
 useradd -ms /bin/bash -U simplified
@@ -27,15 +38,20 @@ echo "if [[ -f $SIMPLIFIED_ENVIRONMENT ]]; then \
       source $SIMPLIFIED_ENVIRONMENT; fi" >> env/bin/activate
 
 # Install Poetry
+curl -sSL https://install.python-poetry.org | POETRY_HOME="/opt/poetry" python3 - --yes --version "1.1.12"
+ln -s /opt/poetry/bin/poetry /bin/poetry
 
 # Install required python libraries.
-
+set +x && source env/bin/activate && set -x
 # Update pip and setuptools.
-
-
+python3 -m pip install -U pip setuptools
+# Install the necessary requirements.
+poetry install --no-dev --no-root -E pg
+poetry cache clear -n --all pypi
 
 # Install NLTK.
-
+python3 -m textblob.download_corpora lite
+mv /root/nltk_data /usr/lib/
 
 # Link the repository code to /home/simplified and change permissions
 su - simplified -c "ln -s /var/www/circulation /home/simplified/circulation"
