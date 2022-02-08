4cfe45e6e721674961325efefd75b56214c72458
Mark Raynsford <code@io7m.com>
2022-02-08 18:02:56 +0000

    Replace static methods with reusable test fixtures

--- tests/core/models/test_licensing.py
+++ tests/core/models/test_licensing.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 import datetime
 import json
-from typing import Optional
+from typing import Callable, Optional
 from unittest.mock import MagicMock, PropertyMock
 
 import pytest
@@ -1575,45 +1575,95 @@ class TestLicensePoolDeliveryMechanism(DatabaseTest):
 
 class TestFormatPriorities:
     @pytest.fixture
-    def sample_data_0(self):
+    def mock_delivery(
+        self,
+    ) -> Callable[[Optional[str], Optional[str]], DeliveryMechanism]:
+        def delivery_mechanism(
+            drm_scheme: Optional[str] = None,
+            content_type: Optional[str] = "application/epub+zip",
+        ) -> DeliveryMechanism:
+            def _delivery_eq(self, other):
+                return (
+                    self.drm_scheme == other.drm_scheme
+                    and self.content_type == other.content_type
+                )
+
+            def _delivery_repr(self):
+                return f"DeliveryMechanism(drm_scheme={self.drm_scheme}, content_type={self.content_type})"
+
+            _delivery = MagicMock(spec=DeliveryMechanism)
+            _delivery.drm_scheme = drm_scheme
+            _delivery.content_type = content_type
+            _delivery.__eq__ = _delivery_eq
+            _delivery.__repr__ = _delivery_repr
+
+            return _delivery
+
+        return delivery_mechanism
+
+    @pytest.fixture
+    def mock_mechanism(
+        self, mock_delivery
+    ) -> Callable[[Optional[str], Optional[str]], LicensePoolDeliveryMechanism]:
+        def mechanism(
+            drm_scheme: Optional[str] = None,
+            content_type: Optional[str] = "application/epub+zip",
+        ) -> LicensePoolDeliveryMechanism:
+            def _mechanism_eq(self, other):
+                return self.delivery_mechanism == other.delivery_mechanism
+
+            def _mechanism_repr(self):
+                return f"LicensePoolDeliveryMechanism(delivery_mechanism={self.delivery_mechanism})"
+
+            _mechanism = MagicMock(spec=LicensePoolDeliveryMechanism)
+            _mechanism.delivery_mechanism = mock_delivery(drm_scheme, content_type)
+            _mechanism.__eq__ = _mechanism_eq
+            _mechanism.__repr__ = _mechanism_repr
+            return _mechanism
+
+        return mechanism
+
+    @pytest.fixture
+    def sample_data_0(self, mock_mechanism):
         """An arrangement of delivery mechanisms taken from a working database."""
-        make = TestFormatPriorities.mechanism_of
         return [
-            make("application/vnd.adobe.adept+xml", "application/epub+zip"),
-            make(
+            mock_mechanism("application/vnd.adobe.adept+xml", "application/epub+zip"),
+            mock_mechanism(
                 "Libby DRM",
                 "application/vnd.overdrive.circulation.api+json;profile=audiobook",
             ),
-            make(None, "application/audiobook+json"),
-            make(
+            mock_mechanism(None, "application/audiobook+json"),
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json", "application/pdf"
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json",
                 "application/epub+zip",
             ),
-            make(None, "application/epub+zip"),
-            make(None, "application/pdf"),
-            make("application/vnd.librarysimplified.findaway.license+json", None),
-            make(
+            mock_mechanism(None, "application/epub+zip"),
+            mock_mechanism(None, "application/pdf"),
+            mock_mechanism(
+                "application/vnd.librarysimplified.findaway.license+json", None
+            ),
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json",
                 "application/audiobook+json",
             ),
-            make(None, "application/kepub+zip"),
-            make(None, "application/x-mobipocket-ebook"),
-            make(None, "application/x-mobi8-ebook"),
-            make(None, "text/plain; charset=utf-8"),
-            make(None, "application/octet-stream"),
-            make(None, "text/html; charset=utf-8"),
-            make(
+            mock_mechanism(None, "application/kepub+zip"),
+            mock_mechanism(None, "application/x-mobipocket-ebook"),
+            mock_mechanism(None, "application/x-mobi8-ebook"),
+            mock_mechanism(None, "text/plain; charset=utf-8"),
+            mock_mechanism(None, "application/octet-stream"),
+            mock_mechanism(None, "text/html; charset=utf-8"),
+            mock_mechanism(
                 "http://www.feedbooks.com/audiobooks/access-restriction",
                 "application/audiobook+json",
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.readium.lcp.license.v1.0+json",
                 "application/audiobook+lcp",
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.readium.lcp.license.v1.0+json", "application/epub+zip"
             ),
         ]
@@ -1626,22 +1676,22 @@ class TestFormatPriorities:
         )
         assert [] == priorities.prioritize_mechanisms([])
 
-    def test_identity_one(self):
+    def test_identity_one(self, mock_mechanism):
         priorities = FormatPriorities(
             prioritized_drm_schemes=[],
             prioritized_content_types=[],
             hidden_content_types=[],
         )
-        mechanism_0 = TestFormatPriorities.mechanism_of()
+        mechanism_0 = mock_mechanism()
         assert [mechanism_0] == priorities.prioritize_mechanisms([mechanism_0])
 
-    def test_hidden_types_excluded(self):
+    def test_hidden_types_excluded(self, mock_mechanism):
         priorities = FormatPriorities(
             prioritized_drm_schemes=[],
             prioritized_content_types=[],
             hidden_content_types=["application/epub+zip"],
         )
-        mechanism_0 = TestFormatPriorities.mechanism_of()
+        mechanism_0 = mock_mechanism()
         assert [] == priorities.prioritize_mechanisms([mechanism_0])
 
     def test_non_prioritized_drm_0(self, sample_data_0):
@@ -1653,52 +1703,54 @@ class TestFormatPriorities:
         expected = sample_data_0.copy()
         assert expected == priorities.prioritize_mechanisms(sample_data_0)
 
-    def test_prioritized_content_type_0(self, sample_data_0):
+    def test_prioritized_content_type_0(self, mock_mechanism, sample_data_0):
         """A simple configuration where an unusual content type is prioritized."""
         priorities = FormatPriorities(
             prioritized_drm_schemes=[],
             prioritized_content_types=["application/x-mobi8-ebook"],
             hidden_content_types=[],
         )
-        make = TestFormatPriorities.mechanism_of
+
         # We expect the mobi8-ebook format to be pushed to the front of the list.
         # All other non-DRM formats are moved to the start of the list in a more or less arbitrary order.
         expected = [
-            make(None, "application/x-mobi8-ebook"),
-            make(None, "application/audiobook+json"),
-            make(None, "application/epub+zip"),
-            make(None, "application/pdf"),
-            make(None, "application/kepub+zip"),
-            make(None, "application/x-mobipocket-ebook"),
-            make(None, "text/plain; charset=utf-8"),
-            make(None, "application/octet-stream"),
-            make(None, "text/html; charset=utf-8"),
-            make("application/vnd.adobe.adept+xml", "application/epub+zip"),
-            make(
+            mock_mechanism(None, "application/x-mobi8-ebook"),
+            mock_mechanism(None, "application/audiobook+json"),
+            mock_mechanism(None, "application/epub+zip"),
+            mock_mechanism(None, "application/pdf"),
+            mock_mechanism(None, "application/kepub+zip"),
+            mock_mechanism(None, "application/x-mobipocket-ebook"),
+            mock_mechanism(None, "text/plain; charset=utf-8"),
+            mock_mechanism(None, "application/octet-stream"),
+            mock_mechanism(None, "text/html; charset=utf-8"),
+            mock_mechanism("application/vnd.adobe.adept+xml", "application/epub+zip"),
+            mock_mechanism(
                 "Libby DRM",
                 "application/vnd.overdrive.circulation.api+json;profile=audiobook",
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json", "application/pdf"
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json",
                 "application/epub+zip",
             ),
-            make("application/vnd.librarysimplified.findaway.license+json", None),
-            make(
+            mock_mechanism(
+                "application/vnd.librarysimplified.findaway.license+json", None
+            ),
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json",
                 "application/audiobook+json",
             ),
-            make(
+            mock_mechanism(
                 "http://www.feedbooks.com/audiobooks/access-restriction",
                 "application/audiobook+json",
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.readium.lcp.license.v1.0+json",
                 "application/audiobook+lcp",
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.readium.lcp.license.v1.0+json", "application/epub+zip"
             ),
         ]
@@ -1707,7 +1759,7 @@ class TestFormatPriorities:
         assert expected == received
         assert len(sample_data_0) == len(received)
 
-    def test_prioritized_content_type_1(self, sample_data_0):
+    def test_prioritized_content_type_1(self, mock_mechanism, sample_data_0):
         """A test of a more aggressive configuration where multiple content types
         and DRM schemes are prioritized."""
         priorities = FormatPriorities(
@@ -1730,39 +1782,40 @@ class TestFormatPriorities:
                 "text/html; charset=utf-8",
             ],
         )
-        make = TestFormatPriorities.mechanism_of
         expected = [
-            make(None, "application/epub+zip"),
-            make(None, "application/audiobook+json"),
-            make(None, "application/pdf"),
-            make(
+            mock_mechanism(None, "application/epub+zip"),
+            mock_mechanism(None, "application/audiobook+json"),
+            mock_mechanism(None, "application/pdf"),
+            mock_mechanism(
                 "application/vnd.readium.lcp.license.v1.0+json", "application/epub+zip"
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.readium.lcp.license.v1.0+json",
                 "application/audiobook+lcp",
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json",
                 "application/epub+zip",
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json",
                 "application/audiobook+json",
             ),
-            make(
+            mock_mechanism(
                 "application/vnd.librarysimplified.bearer-token+json", "application/pdf"
             ),
-            make("application/vnd.adobe.adept+xml", "application/epub+zip"),
-            make(
+            mock_mechanism("application/vnd.adobe.adept+xml", "application/epub+zip"),
+            mock_mechanism(
                 "http://www.feedbooks.com/audiobooks/access-restriction",
                 "application/audiobook+json",
             ),
-            make(
+            mock_mechanism(
                 "Libby DRM",
                 "application/vnd.overdrive.circulation.api+json;profile=audiobook",
             ),
-            make("application/vnd.librarysimplified.findaway.license+json", None),
+            mock_mechanism(
+                "application/vnd.librarysimplified.findaway.license+json", None
+            ),
         ]
         received = priorities.prioritize_mechanisms(sample_data_0)
         assert expected == received
@@ -1778,35 +1831,3 @@ class TestFormatPriorities:
                 item["type"] = mechanism.delivery_mechanism.content_type
             output.append(item)
         print(json.dumps(output, indent=2))
-
-    @staticmethod
-    def mechanism_of(
-        drm_scheme: Optional[str] = None,
-        content_type: Optional[str] = "application/epub+zip",
-    ):
-        def _delivery_eq(self, other):
-            return (
-                self.drm_scheme == other.drm_scheme
-                and self.content_type == other.content_type
-            )
-
-        def _delivery_repr(self):
-            return f"DeliveryMechanism(drm_scheme={self.drm_scheme}, content_type={self.content_type})"
-
-        _delivery = MagicMock(spec=DeliveryMechanism)
-        _delivery.drm_scheme = drm_scheme
-        _delivery.content_type = content_type
-        _delivery.__eq__ = _delivery_eq
-        _delivery.__repr__ = _delivery_repr
-
-        def _mechanism_eq(self, other):
-            return self.delivery_mechanism == other.delivery_mechanism
-
-        def _mechanism_repr(self):
-            return f"LicensePoolDeliveryMechanism(delivery_mechanism={self.delivery_mechanism})"
-
-        _mechanism = MagicMock(spec=LicensePoolDeliveryMechanism)
-        _mechanism.delivery_mechanism = _delivery
-        _mechanism.__eq__ = _mechanism_eq
-        _mechanism.__repr__ = _mechanism_repr
-        return _mechanism
