02318b3f31eb19648efa2b35edf3cf5ed9846688
Rishi Diwan <rishi.diwan@toptal.com>
2022-03-25 12:25:54 +0530

    Test cases for Provider API settings deduping

--- tests/api/admin/controller/test_collection_settings.py
+++ tests/api/admin/controller/test_collection_settings.py
@@ -46,3 +46,45 @@ class TestCollectionSettingsController(DatabaseTest):
         # We want to make sure that the result setting array contains a correct value in a list format.
         saved_affiliation_attributes = settings[affiliation_attributes_key]
         assert expected_affiliation_attributes == saved_affiliation_attributes
+
+    def test_duplicate_protocol_settings(self):
+        """Dedupe protocol settings using the last settings of the same value"""
+        manager = create_autospec(spec=CirculationManager)
+        manager._db = PropertyMock(return_value=self._db)
+
+        class MockProviderAPI:
+            NAME = "NAME"
+            SETTINGS = [
+                dict(key="k1", value="v1"),
+                dict(key="k2", value="v2"),  # This should get overwritten
+                dict(key="k2", value="v3"),  # Only this should remain
+            ]
+
+        controller = CollectionSettingsController(manager)
+        controller.PROVIDER_APIS = [MockProviderAPI]
+        protocols = controller._get_collection_protocols()
+
+        k2_list = list(filter(lambda x: x["key"] == "k2", protocols[0]["settings"]))
+        assert len(k2_list) == 1
+        assert k2_list[0]["value"] == "v3"
+
+        class MockProviderAPIMulti:
+            NAME = "NAME"
+            SETTINGS = [
+                dict(key="k1", value="v0"),  # This should get overwritten
+                dict(key="k1", value="v1"),  # Only this should remain
+                dict(key="k2", value="v1"),  # This should get overwritten
+                dict(key="k2", value="v2"),  # This should get overwritten
+                dict(key="k2", value="v4"),  # Only this should remain
+            ]
+
+        controller.PROVIDER_APIS = [MockProviderAPIMulti]
+        protocols = controller._get_collection_protocols()
+
+        k2_list = list(filter(lambda x: x["key"] == "k2", protocols[0]["settings"]))
+        assert len(k2_list) == 1
+        assert k2_list[0]["value"] == "v4"
+
+        k1_list = list(filter(lambda x: x["key"] == "k1", protocols[0]["settings"]))
+        assert len(k1_list) == 1
+        assert k1_list[0]["value"] == "v1"
