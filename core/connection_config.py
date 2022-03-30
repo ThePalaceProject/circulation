0b84fc6b53fd0d046bdc63dcbd87f53a0aebe655
Mark Raynsford <code@io7m.com>
2022-03-30 08:27:55 +0000

    Ensure configuration key has a unique prefix

--- core/connection_config.py
+++ core/connection_config.py
@@ -8,7 +8,7 @@ class ConnectionConfigurationTrait(ConfigurationTrait):
     """Configuration information for connections to external servers."""
 
     max_retry_count = ConfigurationMetadata(
-        key="max_retry_count",
+        key="connection_max_retry_count",
         label=_("Connection retry limit"),
         description=_(
             "The maximum number of times to retry a request for certain connection-related errors."
