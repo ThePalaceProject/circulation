b9ee00406b654d86f1b69ce19f7619c23bb75c87
Mark Raynsford <code@io7m.com>
2022-04-20 15:04:13 +0000

    Rewrite fetch.py to use admin API endpoints

--- customlists/fetch.py
+++ customlists/fetch.py
@@ -1,83 +1,153 @@
 #!/usr/bin/env python
 
-import logging
 import argparse
-import feedparser
 import json
-import jsonschema
+import logging
 import os
+from typing import List
+
+import feedparser
+import jsonschema
+import requests
 
 logging.basicConfig()
 logger = logging.getLogger()
 
-parser: argparse.ArgumentParser = argparse.ArgumentParser(
-    description="Fetch a custom list."
-)
-parser.add_argument("--server", help="The address of the CM", required=True)
-parser.add_argument("--output", help="The output file", required=True)
-parser.add_argument("--list-name", help="The name of the custom list", required=True)
-parser.add_argument('--verbose', '-v', action='count', default=0,
-                    help="Increase verbosity (can be specified multiple times)")
-args: argparse.Namespace = parser.parse_args()
 
-verbose: int = args.verbose or 0
-if verbose > 0:
-    logger.setLevel(logging.INFO)
-if verbose > 1:
-    logger.setLevel(logging.DEBUG)
+def fatal(message: str) -> None:
+    logging.fatal(message)
+    if __name__ == "__main__":
+        exit(1)
+    else:
+        raise RuntimeError
 
-#
-# Fetch the list's OPDS feed and parse it.
-#
 
-server: str = args.server.rstrip('/')
-server_target: str = f'{server}/lists/{args.list_name}/crawlable'
-logging.info(f'fetching {server_target}')
-feed = feedparser.parse(url_file_stream_or_string=server_target)
+def parse_arguments() -> argparse.Namespace:
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description="Fetch a custom list."
+    )
+    parser.add_argument("--server", help="The address of the CM", required=True)
+    parser.add_argument("--username", help="The CM admin username", required=True)
+    parser.add_argument("--password", help="The CM admin password", required=True)
+    parser.add_argument("--output", help="The output file", required=True)
+    parser.add_argument(
+        "--list-name", help="The name of the custom list", required=True
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help="Increase verbosity (can be specified multiple times)",
+    )
+    return parser.parse_args()
 
-#
-# Construct a customlist document.
-#
 
-document = {}
-document['customlists'] = []
-document['%id'] = 'https://schemas.thepalaceproject.io/customlists/1.0'
+def make_custom_list(
+    session: requests.Session, server_base: str, raw_list: dict
+) -> dict:
+    id: int = raw_list["id"]
+    server_list_endpoint: str = f"{server_base}/admin/custom_list/{id}"
 
-customlists = document['customlists']
+    response = session.get(server_list_endpoint)
+    if response.status_code >= 400:
+        fatal(
+            f"failed to retrieve custom list {id}: {response.status_code} {response.reason}"
+        )
 
-customlist = {}
-customlist['%type'] = 'customlist'
-customlist['books'] = []
-customlist['id'] = feed.feed.id
-customlist['name'] = feed.feed.title
-customlists.append(customlist)
+    books = []
+    feed = feedparser.parse(url_file_stream_or_string=response.content)
+    for entry in feed.entries:
+        book = {}
+        book["%type"] = "book"
+        book["id"] = entry.id
+        book["title"] = entry.title
+        books.append(book)
 
-books = customlist['books']
+    customlist = {
+        "%type": "customlist",
+        "books": books,
+        "id": raw_list["id"],
+        "name": raw_list["name"],
+    }
+    logger.info(f"retrieved {len(books)} books for list {id}")
+    return customlist
 
-for entry in feed.entries:
-    book = {}
-    book['%type'] = 'book'
-    book['id'] = entry.id
-    book['title'] = entry.title
-    customlist['books'].append(book)
 
-logger.debug(f'retrieved {len(books)} books')
+def make_custom_lists_document(session: requests.Session, server_base: str) -> dict:
+    logging.info("fetching lists...")
+    server_lists_endpoint: str = f"{server_base}/admin/custom_lists"
+    response = session.get(server_lists_endpoint)
+    if response.status_code >= 400:
+        fatal(
+            f"failed to retrieve custom lists: {response.status_code} {response.reason}"
+        )
 
-#
-# Validate the customlist document against the schema.
-#
+    raw_document = json.loads(response.content)
+    raw_lists: list = raw_document["custom_lists"] or []
 
-with open("customlists.schema.json", "rb") as schema_file:
-    schema: str = json.load(schema_file)
+    custom_lists: List[dict] = []
+    document = {
+        "customlists": custom_lists,
+        "%id": "https://schemas.thepalaceproject.io/customlists/1.0",
+    }
 
-jsonschema.validate(document, schema)
-logger.debug(f'validated output against schema')
+    for raw_list in raw_lists:
+        custom_lists.append(make_custom_list(session, server_base, raw_list))
 
-output_file: str = args.output
-output_file_tmp: str = output_file + '.tmp'
+    logger.info(f"retrieved {len(custom_lists)} custom lists")
+    return document
 
-serialized: str = json.dumps(document, sort_keys=True, indent=2)
-with open(output_file_tmp, 'wb') as out:
-    out.write(serialized.encode('utf-8'))
 
-os.rename(output_file_tmp, output_file)
+def sign_in(
+    session: requests.Session, server_base: str, email: str, password: str
+) -> None:
+    server_login_endpoint: str = f"{server_base}/admin/sign_in_with_password"
+    headers = {"User-Agent": "circulation-customlists-fetch/1.0"}
+    payload = {"email": email, "password": password}
+
+    logging.info("signing in...")
+    response = session.post(
+        server_login_endpoint, headers=headers, data=payload, allow_redirects=False
+    )
+    if response.status_code >= 400:
+        fatal(f"failed to sign in: {response.status_code} {response.reason}")
+
+
+def save_customlists_document(document: dict, output_file: str) -> None:
+    with open("customlists.schema.json", "rb") as schema_file:
+        schema: str = json.load(schema_file)
+
+    jsonschema.validate(document, schema)
+    logger.info(f"validated output against schema")
+
+    output_file_tmp: str = output_file + ".tmp"
+
+    serialized: str = json.dumps(document, sort_keys=True, indent=2)
+    with open(output_file_tmp, "wb") as out:
+        out.write(serialized.encode("utf-8"))
+
+    os.rename(output_file_tmp, output_file)
+
+
+def main():
+    args = parse_arguments()
+
+    verbose: int = args.verbose or 0
+    if verbose > 0:
+        logger.setLevel(logging.INFO)
+    if verbose > 1:
+        logger.setLevel(logging.DEBUG)
+
+    session = requests.Session()
+    server: str = args.server.rstrip("/")
+
+    sign_in(
+        session=session, server_base=server, email=args.username, password=args.password
+    )
+    document = make_custom_lists_document(session=session, server_base=server)
+    save_customlists_document(document, args.output)
+
+
+if __name__ == "__main__":
+    main()
