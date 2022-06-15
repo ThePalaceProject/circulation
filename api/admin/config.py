c5648acd9124e94cbe605ce33a890e61579316c8
Tim DiLauro <tim.dilauro@gmail.com>
2022-06-15 14:41:52 -0400

    Use Admin UI to v0.0.10.

--- api/admin/config.py
+++ api/admin/config.py
@@ -12,7 +12,7 @@ class Configuration:
 
     APP_NAME = "Palace Collection Manager"
     PACKAGE_NAME = "@thepalaceproject/circulation-admin"
-    PACKAGE_VERSION = "0.0.9"
+    PACKAGE_VERSION = "0.0.10"
 
     STATIC_ASSETS = {
         "admin_js": "circulation-admin.js",
