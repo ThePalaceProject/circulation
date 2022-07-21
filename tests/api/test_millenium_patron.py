63e7a5958d98f984b4f236467d2cef320c75e711
Mark Raynsford <code@io7m.com>
2022-07-21 16:31:55 +0000

    Adjust comment wording

--- tests/api/test_millenium_patron.py
+++ tests/api/test_millenium_patron.py
@@ -154,7 +154,7 @@ class TestMilleniumPatronAPI(DatabaseTest):
         patrondata = PatronData(authorization_identifier="good barcode")
         patrondata = self.api._remote_patron_lookup(patrondata)
 
-        # The barcode is correctly captured from the "MENINX[pu]" element.
+        # The identifier is correctly captured from the "MENINX[pu]" element.
         assert "6666666" == patrondata.permanent_id
         assert "alice" == patrondata.authorization_identifier
         assert "alice" == patrondata.username
