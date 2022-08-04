5a6ec17187deccaed531829d4fdaff8c31d262cb
RishiDiwanTT <90382027+RishiDiwanTT@users.noreply.github.com>
2022-08-04 11:29:23 +0530

    Patron activity sync scheduled job (#475)
    * Patron activity sync scheduled job
    
    Should send out notifications to patron mobile apps that
    - Have a device token
    - Have either a loan or hold active
    - Have potentially stale loan/hold activity data
    
    There is an issue with the current implementation of last_activity_sync_time
    wherein the getter finds the sync time to be stale in 15 minutes and clears out the
    last sync time on access, we may want to change this implementation to not clear out
    the information and add a has_activity_sync_expired() method, allowing each type of
    workflow define when a sync is stale
    
    * Mypy linting fixes
    
    * Typing import error resolved
    
    * Notifications test fix

--- core/util/notifications.py
+++ core/util/notifications.py
@@ -2,18 +2,24 @@ from typing import List
 
 import firebase_admin
 from firebase_admin import credentials, messaging
+from sqlalchemy.orm import Session
 
 from core.config import Configuration
-from core.model.devicetokens import DeviceToken
+from core.model.configuration import ConfigurationSetting
+from core.model.constants import NotificationConstants
+from core.model.devicetokens import DeviceToken, DeviceTokenTypes
 from core.model.edition import Edition
 from core.model.identifier import Identifier
-from core.model.patron import Loan
+from core.model.patron import Loan, Patron
 
 
 class PushNotifications:
     # Should be set to true while unit testing
     TESTING_MODE = False
     _fcm_app = None
+    _base_url = None
+
+    VALID_TOKEN_TYPES = [DeviceTokenTypes.FCM_ANDROID, DeviceTokenTypes.FCM_IOS]
 
     @classmethod
     @property
@@ -25,10 +31,24 @@ class PushNotifications:
         return cls._fcm_app
 
     @classmethod
+    def base_url(cls, _db):
+        if not cls._base_url:
+            cls._base_url = ConfigurationSetting.sitewide(
+                _db, Configuration.BASE_URL_KEY
+            ).value
+        return cls._base_url
+
+    @classmethod
     def send_loan_expiry_message(
         cls, loan: Loan, days_to_expiry, tokens: List[DeviceToken]
     ):
+        """Send a loan expiry reminder to the mobile Apps, with enough information
+        to identify two things
+        - Which loan is being mentioned, in order to correctly deep link
+        - Which patron and make the loans api request with the right authentication"""
         responses = []
+        _db = Session.object_session(loan)
+        url = cls.base_url(_db)
         edition: Edition = loan.license_pool.presentation_edition
         identifier: Identifier = loan.license_pool.identifier
         library_short_name = loan.library and loan.library.short_name
@@ -38,6 +58,10 @@ class PushNotifications:
                 data=dict(
                     title=f"Only {days_to_expiry} {'days' if days_to_expiry != 1 else 'day'} left on your loan!",
                     body=f"Your loan on {edition.title} is expiring soon",
+                    event_type=NotificationConstants.LOAN_EXPIRY_TYPE,
+                    loans_endpoint=f"{url}/{loan.library.short_name}/loans",
+                    external_identifier=loan.patron.external_identifier,
+                    authorization_identifier=loan.patron.authorization_identifier,
                     identifier=identifier.identifier,
                     type=identifier.type,
                     library=library_short_name,
@@ -47,3 +71,37 @@ class PushNotifications:
             resp = messaging.send(msg, dry_run=cls.TESTING_MODE, app=cls.fcm_app)
             responses.append(resp)
         return responses
+
+    @classmethod
+    def send_activity_sync_message(cls, patrons: List[Patron]):
+        """Send notifications to the given patrons to sync their bookshelves
+        Enough information needs to be sent to identify a patron on the mobile Apps
+        and make the loans api request with the right authentication"""
+        if not patrons:
+            return []
+
+        msgs = []
+        _db = Session.object_session(patrons[0])
+        url = cls.base_url(_db)
+        for patron in patrons:
+            tokens = [
+                token
+                for token in patron.device_tokens
+                if token.token_type in cls.VALID_TOKEN_TYPES
+            ]
+            loans_api = f"{url}/{patron.library.short_name}/loans"
+            for token in tokens:
+                msg = messaging.Message(
+                    token=token.device_token,
+                    data=dict(
+                        event_type=NotificationConstants.ACTIVITY_SYNC_TYPE,
+                        loans_endpoint=loans_api,
+                        external_identifier=patron.external_identifier,
+                        authorization_identifier=patron.authorization_identifier,
+                    ),
+                )
+                msgs.append(msg)
+        batch: messaging.BatchResponse = messaging.send_all(
+            msgs, dry_run=cls.TESTING_MODE, app=cls.fcm_app
+        )
+        return [resp.message_id for resp in batch.responses]
--- tests/core/util/test_notifications.py
+++ tests/core/util/test_notifications.py
@@ -1,6 +1,9 @@
 from unittest import mock
 
-from core.model import get_one_or_create
+from core.config import Configuration
+from core.model import create, get_one_or_create
+from core.model.configuration import ConfigurationSetting
+from core.model.constants import NotificationConstants
 from core.model.devicetokens import DeviceToken, DeviceTokenTypes
 from core.model.work import Work
 from core.testing import DatabaseTest
@@ -9,8 +12,10 @@ from core.util.notifications import PushNotifications
 
 class TestPushNotifications(DatabaseTest):
     def setup_method(self):
+        super().setup_method()
         PushNotifications.TESTING_MODE = True
-        return super().setup_method()
+        setting = ConfigurationSetting.sitewide(self._db, Configuration.BASE_URL_KEY)
+        setting.value = "http://localhost"
 
     @mock.patch("core.util.notifications.PushNotifications.fcm_app")
     @mock.patch("core.util.notifications.messaging")
@@ -36,6 +41,10 @@ class TestPushNotifications(DatabaseTest):
                 "data": dict(
                     title="Only 1 day left on your loan!",
                     body=f"Your loan on {work.presentation_edition.title} is expiring soon",
+                    event_type=NotificationConstants.LOAN_EXPIRY_TYPE,
+                    loans_endpoint="http://localhost/default/loans",
+                    external_identifier=patron.external_identifier,
+                    authorization_identifier=patron.authorization_identifier,
                     identifier=work.presentation_edition.primary_identifier.identifier,
                     type=work.presentation_edition.primary_identifier.type,
                     library=loan.library.short_name,
@@ -48,3 +57,75 @@ class TestPushNotifications(DatabaseTest):
             (messaging.Message(),),
             {"dry_run": True, "app": mock_fcm},
         ]
+
+    @mock.patch("core.util.notifications.PushNotifications.fcm_app")
+    @mock.patch("core.util.notifications.messaging")
+    def test_send_activity_sync(
+        self, messaging: mock.MagicMock, fcm_app: mock.MagicMock
+    ):
+        patron1 = self._patron()
+        patron2 = self._patron()
+        patron3 = self._patron()
+
+        tokens = []
+        for patron in (patron1, patron2, patron3):
+            t, _ = create(
+                self._db,
+                DeviceToken,
+                patron=patron,
+                device_token=f"ios-token-{patron.id}",
+                token_type=DeviceTokenTypes.FCM_IOS,
+            )
+            tokens.append(t)
+            t, _ = create(
+                self._db,
+                DeviceToken,
+                patron=patron,
+                device_token=f"android-token-{patron.id}",
+                token_type=DeviceTokenTypes.FCM_ANDROID,
+            )
+            tokens.append(t)
+
+        # Notify 2 patrons of 3 total
+        PushNotifications.send_activity_sync_message([patron1, patron2])
+        assert messaging.Message.call_count == 4
+        assert messaging.Message.call_args_list == [
+            mock.call(
+                token=tokens[0].device_token,
+                data=dict(
+                    event_type=NotificationConstants.ACTIVITY_SYNC_TYPE,
+                    loans_endpoint="http://localhost/default/loans",
+                    external_identifier=patron1.external_identifier,
+                    authorization_identifier=patron1.authorization_identifier,
+                ),
+            ),
+            mock.call(
+                token=tokens[1].device_token,
+                data=dict(
+                    event_type=NotificationConstants.ACTIVITY_SYNC_TYPE,
+                    loans_endpoint="http://localhost/default/loans",
+                    external_identifier=patron1.external_identifier,
+                    authorization_identifier=patron1.authorization_identifier,
+                ),
+            ),
+            mock.call(
+                token=tokens[2].device_token,
+                data=dict(
+                    event_type=NotificationConstants.ACTIVITY_SYNC_TYPE,
+                    loans_endpoint="http://localhost/default/loans",
+                    external_identifier=patron2.external_identifier,
+                    authorization_identifier=patron2.authorization_identifier,
+                ),
+            ),
+            mock.call(
+                token=tokens[3].device_token,
+                data=dict(
+                    event_type=NotificationConstants.ACTIVITY_SYNC_TYPE,
+                    loans_endpoint="http://localhost/default/loans",
+                    external_identifier=patron2.external_identifier,
+                    authorization_identifier=patron2.authorization_identifier,
+                ),
+            ),
+        ]
+
+        assert messaging.send_all.call_count == 1
