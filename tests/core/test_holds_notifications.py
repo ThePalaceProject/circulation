a20297003617cfd5b8cc87b95ae13fba856dbf13
Rishi Diwan <rishi.diwan@toptal.com>
2022-08-09 12:36:54 +0530

    Holds notifications for patrons
    The script will only pick holds that have position=0
    There is currently no way of telling if a notification has been sent to a user or not
    So if a hold remains in reserve for a long duration the patron will receive multiple notifications
    as and when the job runs

---
+++ tests/core/test_holds_notifications.py
@@ -0,0 +1,37 @@
+from unittest.mock import call, patch
+
+from core.jobs.holds_notification import HoldsNotificationMonitor
+from core.testing import DatabaseTest
+
+
+class TestHoldsNotifications(DatabaseTest):
+    def setup_method(self):
+        super().setup_method()
+        self.monitor = HoldsNotificationMonitor(self._db)
+
+    def test_item_query(self):
+        patron1 = self._patron()
+        work1 = self._work(with_license_pool=True)
+        work2 = self._work(with_license_pool=True)
+        work3 = self._work(with_license_pool=True)
+        work4 = self._work(with_license_pool=True)
+        hold1, _ = work1.active_license_pool().on_hold_to(patron1, position=1)
+        hold2, _ = work2.active_license_pool().on_hold_to(patron1, position=0)
+        hold3, _ = work3.active_license_pool().on_hold_to(patron1, position=0)
+        hold4, _ = work4.active_license_pool().on_hold_to(patron1, position=None)
+
+        # Only position 0 holds should be pushed queried for
+        assert self.monitor.item_query().all() == [hold2, hold3]
+
+    @patch("core.jobs.holds_notification.PushNotifications")
+    def test_script_run(self, mock_notf):
+        patron1 = self._patron()
+        work1 = self._work(with_license_pool=True)
+        work2 = self._work(with_license_pool=True)
+        hold1, _ = work1.active_license_pool().on_hold_to(patron1, position=0)
+        hold2, _ = work2.active_license_pool().on_hold_to(patron1, position=0)
+        self.monitor.run()
+        assert mock_notf.send_holds_notifications.call_count == 1
+        assert mock_notf.send_holds_notifications.call_args_list == [
+            call([hold1, hold2])
+        ]
