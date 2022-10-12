6083dc01ca7e38677453423881e5b8c7759e39d9
Ray Lee <ray.lee@lyrasis.org>
2022-10-12 10:57:36 -0400

    Update circulation-admin version to 0.4.1.

--- api/admin/config.py
+++ api/admin/config.py
@@ -12,7 +12,7 @@ class Configuration:
 
     APP_NAME = "Palace Collection Manager"
     PACKAGE_NAME = "@thepalaceproject/circulation-admin"
-    PACKAGE_VERSION = "0.4.0"
+    PACKAGE_VERSION = "0.4.1"
 
     STATIC_ASSETS = {
         "admin_js": "circulation-admin.js",
