e923492c7c3fdd749a198fc99ffa9caa8ed45e02
Mark Raynsford <code@io7m.com>
2022-11-08 16:10:11 +0000

    Continue updating tests to use new fixtures
    Affects: https://www.notion.so/lyrasis/Refactor-circulation-tests-to-use-pytest-fixtures-4f2603de236746aeaab123dcf12aa445

--- tests/core_new/conftest.py
+++ tests/core_new/conftest.py
@@ -3,6 +3,7 @@ pytest_plugins = [
     "tests.fixtures.database",
     "tests.fixtures.marc_files",
     "tests.fixtures.overdrive",
+    "tests.fixtures.s3",
     "tests.fixtures.sample_covers",
     "tests.fixtures.search",
     "tests.fixtures.time",
