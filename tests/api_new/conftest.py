81982b5bc77f55fb1a07a2e22090a4a2f6d0e1af
Mark Raynsford <code@io7m.com>
2022-12-06 14:43:56 +0000

    Convert API tests to new test fixtures
    Affects: https://www.notion.so/lyrasis/Refactor-API-test-suite-9852f7a8e4484cc6bed8235acb20a7fe

---
+++ tests/api_new/conftest.py
@@ -0,0 +1,13 @@
+pytest_plugins = [
+    "tests.fixtures.csv_files",
+    "tests.fixtures.database",
+    "tests.fixtures.marc_files",
+    "tests.fixtures.opds2_files",
+    "tests.fixtures.opds_files",
+    "tests.fixtures.overdrive",
+    "tests.fixtures.s3",
+    "tests.fixtures.sample_covers",
+    "tests.fixtures.search",
+    "tests.fixtures.time",
+    "tests.fixtures.vendor_id",
+]
