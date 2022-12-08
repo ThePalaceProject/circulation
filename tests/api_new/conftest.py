de00c1f06bbcadf4fa06e96fb89bcb4b0224009b
Mark Raynsford <code@io7m.com>
2022-12-08 12:13:35 +0000

    Convert API tests to new test fixtures
    Affects: https://www.notion.so/lyrasis/Refactor-API-test-suite-9852f7a8e4484cc6bed8235acb20a7fe

--- tests/api_new/conftest.py
+++ tests/api_new/conftest.py
@@ -1,4 +1,5 @@
 pytest_plugins = [
+    "tests.fixtures.api_axis_files",
     "tests.fixtures.api_bibliotheca_files",
     "tests.fixtures.api_enki_files",
     "tests.fixtures.api_feedbooks_files",
