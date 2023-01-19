d06f71173023834bc33a8e3a884b383c8b9cbac2
Jonathan Green <jonathan@razzard.com>
2023-01-19 16:58:31 -0400

    Add back in override class.

--- tests/api/test_controller_multilib.py
+++ tests/api/test_controller_multilib.py
@@ -1,25 +1,51 @@
+import pytest
+
+from core.model import Collection, ExternalIntegration, get_one_or_create
 from tests.fixtures.api_controller import CirculationControllerFixture
+from tests.fixtures.database import DatabaseTransactionFixture
+from tests.fixtures.vendor_id import VendorIDFixture
+
+
+@pytest.fixture
+def multiple_circulation_fixture(
+    db: DatabaseTransactionFixture, vendor_id_fixture: VendorIDFixture
+):
+    class MultipleLibraryFixture(CirculationControllerFixture):
+        def make_default_libraries(self, _db):
+            return [self.db.library() for x in range(2)]
+
+        def make_default_collection(self, _db, library):
+            collection, ignore = get_one_or_create(
+                self.db.session,
+                Collection,
+                name=f"{self.db.fresh_str()} (for multi-library test)",
+            )
+            collection.create_external_integration(ExternalIntegration.OPDS_IMPORT)
+            library.collections.append(collection)
+            return collection
+
+    return MultipleLibraryFixture(db, vendor_id_fixture)
 
 
 class TestMultipleLibraries:
-    def test_authentication(self, circulation_fixture: CirculationControllerFixture):
+    def test_authentication(
+        self, multiple_circulation_fixture: CirculationControllerFixture
+    ):
         """It's possible to authenticate with multiple libraries and make a
         request that runs in the context of each different library.
         """
-        circulation_fixture.libraries.append(circulation_fixture.db.library())
-
-        l1, l2 = circulation_fixture.libraries
+        l1, l2 = multiple_circulation_fixture.libraries
         assert l1 != l2
-        for library in circulation_fixture.libraries:
-            headers = dict(Authorization=circulation_fixture.valid_auth)
-            with circulation_fixture.request_context_with_library(
+        for library in multiple_circulation_fixture.libraries:
+            headers = dict(Authorization=multiple_circulation_fixture.valid_auth)
+            with multiple_circulation_fixture.request_context_with_library(
                 "/", headers=headers, library=library
             ):
                 patron = (
-                    circulation_fixture.manager.loans.authenticated_patron_from_request()
+                    multiple_circulation_fixture.manager.loans.authenticated_patron_from_request()
                 )
                 assert library == patron.library
-                response = circulation_fixture.manager.index_controller()
+                response = multiple_circulation_fixture.manager.index_controller()
                 assert (
                     "http://cdn/%s/groups/" % library.short_name
                     == response.headers["location"]
