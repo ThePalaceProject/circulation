3d6b28dbb2aa3fc1e33090c7b74e287d10654566
Mark Raynsford <code@io7m.com>
2023-01-20 11:41:24 +0000

    Convert API tests to new test fixtures
    Fix the multilib test. Thanks @jonathangreen!
    
    Affects: https://www.notion.so/lyrasis/Refactor-API-test-suite-9852f7a8e4484cc6bed8235acb20a7fe

--- tests/api/test_controller_multilib.py
+++ tests/api/test_controller_multilib.py
@@ -1,51 +1,48 @@
-import pytest
-
 from core.model import Collection, ExternalIntegration, get_one_or_create
-from tests.fixtures.api_controller import CirculationControllerFixture
-from tests.fixtures.database import DatabaseTransactionFixture
-from tests.fixtures.vendor_id import VendorIDFixture
+from tests.fixtures.api_controller import (
+    CirculationControllerFixture,
+    ControllerFixtureSetupOverrides,
+)
 
 
-@pytest.fixture
-def multiple_circulation_fixture(
-    db: DatabaseTransactionFixture, vendor_id_fixture: VendorIDFixture
-):
-    class MultipleLibraryFixture(CirculationControllerFixture):
-        def make_default_libraries(self, _db):
-            return [self.db.library() for x in range(2)]
+class TestMultipleLibraries:
+    def test_authentication(self, controller_fixture: CirculationControllerFixture):
+        """It's possible to authenticate with multiple libraries and make a
+        request that runs in the context of each different library.
+        """
 
-        def make_default_collection(self, _db, library):
+        def make_default_libraries(_db):
+            return [controller_fixture.db.library() for x in range(2)]
+
+        def make_default_collection(_db, library):
             collection, ignore = get_one_or_create(
-                self.db.session,
+                controller_fixture.db.session,
                 Collection,
-                name=f"{self.db.fresh_str()} (for multi-library test)",
+                name=f"{controller_fixture.db.fresh_str()} (for multi-library test)",
             )
             collection.create_external_integration(ExternalIntegration.OPDS_IMPORT)
             library.collections.append(collection)
             return collection
 
-    return MultipleLibraryFixture(db, vendor_id_fixture)
+        controller_fixture.circulation_manager_setup(
+            overrides=ControllerFixtureSetupOverrides(
+                make_default_collection=make_default_collection,
+                make_default_libraries=make_default_libraries,
+            )
+        )
 
-
-class TestMultipleLibraries:
-    def test_authentication(
-        self, multiple_circulation_fixture: CirculationControllerFixture
-    ):
-        """It's possible to authenticate with multiple libraries and make a
-        request that runs in the context of each different library.
-        """
-        l1, l2 = multiple_circulation_fixture.libraries
+        l1, l2 = controller_fixture.libraries
         assert l1 != l2
-        for library in multiple_circulation_fixture.libraries:
-            headers = dict(Authorization=multiple_circulation_fixture.valid_auth)
-            with multiple_circulation_fixture.request_context_with_library(
+        for library in controller_fixture.libraries:
+            headers = dict(Authorization=controller_fixture.valid_auth)
+            with controller_fixture.request_context_with_library(
                 "/", headers=headers, library=library
             ):
                 patron = (
-                    multiple_circulation_fixture.manager.loans.authenticated_patron_from_request()
+                    controller_fixture.manager.loans.authenticated_patron_from_request()
                 )
                 assert library == patron.library
-                response = multiple_circulation_fixture.manager.index_controller()
+                response = controller_fixture.manager.index_controller()
                 assert (
                     "http://cdn/%s/groups/" % library.short_name
                     == response.headers["location"]
