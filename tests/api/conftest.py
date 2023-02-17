b3d374a109be93f82aaa0e7ba92142c9701f8e3f
Mark Raynsford <code@io7m.com>
2023-02-17 18:51:57 +0000

    Fix a lint issue.

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,8 +1,8 @@
 from pytest import register_assert_rewrite
 
-register_assert_rewrite('tests.fixtures.database')
-register_assert_rewrite('tests.fixtures.files')
-register_assert_rewrite('tests.fixtures.vendor_id')
+register_assert_rewrite("tests.fixtures.database")
+register_assert_rewrite("tests.fixtures.files")
+register_assert_rewrite("tests.fixtures.vendor_id")
 
 pytest_plugins = [
     "core.testing",
