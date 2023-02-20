fbbc6ac1d3700f09d0020270cdc4691bcc6ef336
Porin Custic <porin.custic@gmail.com>
2023-02-20 13:42:08 +0100

    Use alembic for migration

--- migration/20230214-remove-admin-credential.sql
+++
@@ -1,6 +0,0 @@
---
--- Remove admin credential field since we are not using any external admin auth service.
---
-
--- Remove the admin.credential
-ALTER TABLE admins DROP COLUMN if exists credential;
