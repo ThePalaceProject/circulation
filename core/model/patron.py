5df2977c849ea9a1903c05e7e330209cbc98617f
Rishi Diwan <rishi.diwan@toptal.com>
2023-03-21 15:13:47 +0530

    Mypy linting fix

--- core/model/patron.py
+++ core/model/patron.py
@@ -30,6 +30,7 @@ from . import Base, get_one_or_create, numericrange_to_tuple
 from .credential import Credential
 
 if TYPE_CHECKING:
+    from core.model import IntegrationClient  # noqa: autoflake
     from core.model.library import Library  # noqa: autoflake
     from core.model.licensing import LicensePool  # noqa: autoflake
 
@@ -568,6 +569,11 @@ class Hold(Base, LoanAndHoldMixin):
     end = Column(DateTime(timezone=True), index=True)
     position = Column(Integer, index=True)
     external_identifier = Column(Unicode, unique=True, nullable=True)
+
+    patron = relationship("Patron", back_populates="holds", lazy="joined")
+    integration_client = relationship(
+        "IntegrationClient", back_populates="holds", lazy="joined"
+    )
 
     def __lt__(self, other):
         return self.id < other.id
