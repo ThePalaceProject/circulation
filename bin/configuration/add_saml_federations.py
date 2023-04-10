500ebec514553665b86000f4414c190c18bef0e9
Tim DiLauro <tdilauro@users.noreply.github.com>
2023-04-10 13:52:41 -0400

    Provides a configuration script to add SAML federations to the CM. (#988)

---
+++ bin/configuration/add_saml_federations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Adds SAML federation metadata to `samlfederations` table."""
+
+import os
+import sys
+from contextlib import closing
+
+bin_dir = os.path.split(__file__)[0]
+package_dir = os.path.join(bin_dir, "..", "..")
+sys.path.append(os.path.abspath(package_dir))
+
+from api.saml.metadata.federations import incommon
+from api.saml.metadata.federations.model import SAMLFederation
+from core.model import production_session
+
+with closing(production_session()) as db:
+    incommon_federation = (
+        db.query(SAMLFederation)
+        .filter(SAMLFederation.type == incommon.FEDERATION_TYPE)
+        .one_or_none()
+    )
+
+    if not incommon_federation:
+        incommon_federation = SAMLFederation(
+            incommon.FEDERATION_TYPE,
+            incommon.IDP_METADATA_SERVICE_URL,
+            incommon.CERTIFICATE,
+        )
+
+        db.add(incommon_federation)
+        db.commit()
