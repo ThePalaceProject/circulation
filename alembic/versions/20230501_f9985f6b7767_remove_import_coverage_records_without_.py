e0a25ba845ca9accc08e1f2f233911e589bf0b5d
Rishi Diwan <rishi.diwan@toptal.com>
2023-05-02 12:51:35 +0530

    Fix alembic head conflict

--- alembic/versions/20230501_f9985f6b7767_remove_import_coverage_records_without_.py
+++ alembic/versions/20230501_f9985f6b7767_remove_import_coverage_records_without_.py
@@ -1,7 +1,7 @@
 """Remove import coverage records without collections
 
 Revision ID: f9985f6b7767
-Revises: 3ee5b99f2ae7
+Revises: 5dcbc92c20b2
 Create Date: 2023-05-01 10:07:45.737475+00:00
 
 """
@@ -9,7 +9,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "f9985f6b7767"
-down_revision = "3ee5b99f2ae7"
+down_revision = "5dcbc92c20b2"
 branch_labels = None
 depends_on = None
 
