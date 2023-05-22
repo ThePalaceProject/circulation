431fadd104f33ff47150071618beec4d7169a669
Rishi Diwan <rishi.diwan@toptal.com>
2023-05-22 16:24:21 +0530

    Mypy fixes

--- core/crypt/aes.py
+++ core/crypt/aes.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
@@ -5,16 +7,19 @@ from core.crypt import CryptBase
 
 
 class CryptAESCBC(CryptBase):
+    """AES MODE_CBC based encryption"""
+
     IV_LENGTH = 16
     KEY_LENGTH = 32
     PADDING = 16
 
-    def __init__(self, key: bytes, iv: bytes = None) -> None:
+    def __init__(self, key: bytes, iv: bytes | None = None) -> None:
         self._iv = iv
         self.key = key
 
     @classmethod
     def generate_key(cls):
+        """Generate a random bytes key of a set length"""
         return get_random_bytes(cls.KEY_LENGTH)
 
     @property
@@ -23,29 +28,44 @@ class CryptAESCBC(CryptBase):
             self._iv = get_random_bytes(self.IV_LENGTH)
         return self._iv
 
-    def _pad_content(self, content: bytes) -> bytes:
-        pad_length = self.PADDING - len(content) % self.PADDING
-        if pad_length == 0:
-            pad_length = self.PADDING
-        return content + pad_length.to_bytes(1, "big") * pad_length
-
-    def _unpad_content(self, content: bytes) -> bytes:
-        pad_byte = content[-1]
-        return content[:-pad_byte]
-
     @iv.setter
     def iv(self, value):
         if len(value) != self.IV_LENGTH:
             raise ValueError(f"IV length must be {self.IV_LENGTH} bytes.")
         self._iv = value
 
+    def _pad_content(self, content: bytes) -> bytes:
+        """Pad content to be encrpyted using the PKCS#7 protocol
+        :param content: Content to be padded to a set block size
+        """
+        pad_length = self.PADDING - len(content) % self.PADDING
+        if pad_length == 0:
+            pad_length = self.PADDING
+        return content + pad_length.to_bytes(1, "big") * pad_length
+
+    def _unpad_content(self, content: bytes) -> bytes:
+        """Unpad content, generally used after decryption
+        :param content: Content to be unpadded
+        """
+        pad_byte = content[-1]
+        return content[:-pad_byte]
+
     def encrypt(self, content: bytes) -> bytes:
+        """Encrypt a given block of content.
+        The content will be padded before encryption.
+        After encryption the IV will be pre-prended.
+        :param content: Content to be encrypted
+        """
         cipher = AES.new(self.key, AES.MODE_CBC, iv=self.iv)
         padded = self._pad_content(content)
         encrypted = cipher.encrypt(padded)
         return self.iv + encrypted
 
     def decrypt(self, content: bytes) -> bytes:
+        """Decrypt content.
+        The method assumes the IV is pre-prended and PKCS#7 content padding has been done.
+        :param content: Content to be decrypted
+        """
         self.iv, encrypted = content[: self.IV_LENGTH], content[self.IV_LENGTH :]
         cipher = AES.new(self.key, AES.MODE_CBC, iv=self.iv)
         decrypted = cipher.decrypt(encrypted)
