09b208cf8162f726f0f71057c512ccd242f213b1
Mark Raynsford <code@io7m.com>
2023-06-06 11:03:13 +0000

    Add a database migration to set license counts for OPDS4D collections.

---
+++ alembic/versions/20230606_28717fc6e50f_opds_for_distributors_unlimited_access.py
@@ -0,0 +1,56 @@
+"""opds for distributors unlimited access
+
+Revision ID: 28717fc6e50f
+Revises: a9ed3f76d649
+Create Date: 2023-06-06 10:08:35.892018+00:00
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '28717fc6e50f'
+down_revision = 'a9ed3f76d649'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    connection = op.get_bind()
+    connection.execute("""
+  UPDATE
+    licensepools AS lp
+  SET
+    licenses_owned     = -1,
+    licenses_available = -1
+  FROM
+    collections c,
+    externalintegrations e
+  WHERE
+    lp.licenses_owned             = 1
+    and lp.licenses_available     = 1
+    and lp.collection_id          = c.id
+    and c.external_integration_id = e.id
+    and e.protocol                = 'OPDS for Distributors'
+    """)
+    pass
+
+
+def downgrade() -> None:
+    connection = op.get_bind()
+    connection.execute("""
+  UPDATE
+    licensepools AS lp
+  SET
+    licenses_owned     = 1,
+    licenses_available = 1
+  FROM
+    collections c,
+    externalintegrations e
+  WHERE
+    lp.licenses_owned             = -1
+    and lp.licenses_available     = -1
+    and lp.collection_id          = c.id
+    and c.external_integration_id = e.id
+    and e.protocol                = 'OPDS for Distributors'
+    """)
+    pass
