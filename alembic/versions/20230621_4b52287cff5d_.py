c76ccf8e0c5e7d9a4f6f0deab3397202da89ea7e
Rishi Diwan <rishi.diwan@toptal.com>
2023-06-21 17:58:08 +0530

    Merging alembic branchpoints

---
+++ alembic/versions/20230621_4b52287cff5d_.py
@@ -0,0 +1,22 @@
+"""Merging branches
+
+Revision ID: 4b52287cff5d
+Revises: 0a1c9c3f5dd2, 0af587ff8595
+Create Date: 2023-06-21 12:16:22.170558+00:00
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "4b52287cff5d"
+down_revision = ("0a1c9c3f5dd2", "0af587ff8595")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
