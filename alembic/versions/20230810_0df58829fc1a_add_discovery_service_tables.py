4f26759ebb4807a2a9b74a8ce8afac7140e0408e
Jonathan Green <jonathan@razzard.com>
2023-08-10 14:58:27 -0300

    Write complete migration

--- alembic/versions/20230810_0df58829fc1a_add_discovery_service_tables.py
+++ alembic/versions/20230810_0df58829fc1a_add_discovery_service_tables.py
@@ -8,6 +8,13 @@ Create Date: 2023-08-10 15:49:36.784169+00:00
 import sqlalchemy as sa
 
 from alembic import op
+from api.discovery.opds_registration import OpdsRegistrationService
+from core.migration.migrate_external_integration import (
+    _migrate_external_integration,
+    get_configuration_settings,
+    get_integrations,
+    get_library_for_integration,
+)
 from core.migration.util import drop_enum, pg_update_enum
 
 # revision identifiers, used by Alembic.
@@ -58,8 +65,76 @@ def upgrade() -> None:
         new_goals_enum,
     )
 
+    # Migrate data
+    connection = op.get_bind()
+    integrations = get_integrations(connection, "discovery")
+    for integration in integrations:
+        # This should always be the case, but we want to make sure
+        assert integration.protocol == "OPDS Registration"
+
+        # Create the settings and library settings dicts from the configurationsettings
+        settings_dict, library_settings, self_test_result = get_configuration_settings(
+            connection, integration
+        )
+
+        # Write the configurationsettings into the integration_configurations table
+        integration_configuration_id = _migrate_external_integration(
+            connection,
+            integration,
+            OpdsRegistrationService,
+            "DISCOVERY_GOAL",
+            settings_dict,
+            self_test_result,
+        )
+
+        # Write the library settings into the discovery_service_registrations table
+        interation_libraries = get_library_for_integration(
+            connection, integration_configuration_id
+        )
+
+        vendor_id = settings_dict.get("vendor_id")
+        for library_id in interation_libraries:
+            library_settings_dict = library_settings[library_id]
+
+            status = library_settings_dict.get("library-registration-status")
+            if status is None:
+                status = "FAILURE"
+            else:
+                status = status.upper()
+
+            stage = library_settings_dict.get("library-registration-stage")
+            if stage is None:
+                stage = "TESTING"
+            else:
+                stage = stage.upper()
+
+            web_client = library_settings_dict.get("library-registration-web-client")
+            short_name = library_settings_dict.get("username")
+            shared_secret = library_settings_dict.get("password")
+
+            connection.execute(
+                "insert into discovery_service_registrations "
+                "(status, stage, web_client, short_name, shared_secret, integration_id, library_id, vendor_id) "
+                "values (%s, %s, %s, %s, %s, %s, %s, %s)",
+                (
+                    status,
+                    stage,
+                    web_client,
+                    short_name,
+                    shared_secret,
+                    integration_configuration_id,
+                    library_id,
+                    vendor_id,
+                ),
+            )
+
 
 def downgrade() -> None:
+    connection = op.get_bind()
+    connection.execute(
+        "DELETE from integration_configurations where goal = %s", "DISCOVERY_GOAL"
+    )
+
     op.drop_table("discovery_service_registrations")
     drop_enum(op, "registrationstatus")
     drop_enum(op, "registrationstage")
