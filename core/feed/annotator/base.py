8bccb3dd8372dc3476b2b5898d532a0b4d5deccb
RishiDiwanTT <90382027+RishiDiwanTT@users.noreply.github.com>
2023-09-19 17:43:07 +0530

    Comment fix

--- core/feed/annotator/base.py
+++ core/feed/annotator/base.py
@@ -95,7 +95,7 @@ class ToFeedEntry:
         name = contributor.display_name or contributor.sort_name
         name_key = name and name.lower()
         if not name_key or name_key in state[marc_role]:
-            # Either there is not valid name present or
+            # Either there is no valid name present or
             # we've already credited this person with this
             # MARC role. Returning a tag would be redundant.
             return None
