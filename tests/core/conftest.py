9227e427a30492e1a7ea9f2baa1bc68e4dd9e898
Jonathan Green <jonathan@razzard.com>
2023-10-04 16:17:19 -0300

    Remove the overdrive fixture from core

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -5,7 +5,6 @@ pytest_plugins = [
     "tests.fixtures.library",
     "tests.fixtures.opds2_files",
     "tests.fixtures.opds_files",
-    "tests.fixtures.overdrive",
     "tests.fixtures.s3",
     "tests.fixtures.sample_covers",
     "tests.fixtures.search",
