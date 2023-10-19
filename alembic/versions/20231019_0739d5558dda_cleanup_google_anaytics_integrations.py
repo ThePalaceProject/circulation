abed5bb53235d757a1a96cae605394f68ee2b3ec
Rishi Diwan <rishi.diwan@toptal.com>
2023-10-19 12:16:35 +0530

    Data migration to nuke all google analytics provider data

---
+++ alembic/versions/20231019_0739d5558dda_cleanup_google_anaytics_integrations.py
@@ -0,0 +1,47 @@
+"""Cleanup google anaytics integrations
+
+Revision ID: 0739d5558dda
+Revises: 21a65b8f391d
+Create Date: 2023-10-19 05:23:00.694886+00:00
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0739d5558dda"
+down_revision = "21a65b8f391d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Remove all references to google analytics providers from the DB"""
+    conn = op.get_bind()
+    rows = conn.execute(
+        "SELECT id from externalintegrations where goal='analytics' and protocol='api.google_analytics_provider';"
+    ).all()
+    analytics_ids = tuple(r[0] for r in rows)
+
+    if len(analytics_ids):
+        conn.execute(
+            sa.text(
+                "DELETE from externalintegrations_libraries where externalintegration_id IN :id_list;"
+            ),
+            id_list=analytics_ids,
+        )
+        conn.execute(
+            sa.text(
+                "DELETE from configurationsettings where external_integration_id IN :id_list"
+            ),
+            id_list=analytics_ids,
+        )
+        conn.execute(
+            sa.text("DELETE from externalintegrations where id IN :id_list;"),
+            id_list=analytics_ids,
+        )
+
+
+def downgrade() -> None:
+    pass
