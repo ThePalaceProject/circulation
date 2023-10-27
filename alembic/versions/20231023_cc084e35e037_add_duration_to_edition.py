8af08e11d2584c809a340c290a4114f66194de00
Rishi Diwan <rishi.diwan@toptal.com>
2023-10-27 12:39:37 +0530

    Chaanged the alembic revision history

--- alembic/versions/20231023_cc084e35e037_add_duration_to_edition.py
+++ alembic/versions/20231023_cc084e35e037_add_duration_to_edition.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "cc084e35e037"
-down_revision = "0739d5558dda"
+down_revision = "7fceb9488bc6"
 branch_labels = None
 depends_on = None
 
