8cf8b63b6e68e03ab7d18cbbc955290cc6ee5381
Daniel Bernstein <dabylon@gmail.com>
2023-12-01 17:44:59 -0800

    Change the generate_quicksight_url command params to library_uuids from library_ids since library ids are not exposed in the API (and it is probably better that they aren't.

--- api/admin/model/quicksight.py
+++ api/admin/model/quicksight.py
@@ -1,4 +1,5 @@
 from typing import List
+from uuid import UUID
 
 from pydantic import Field, validator
 
@@ -6,13 +7,15 @@ from core.util.flask_util import CustomBaseModel, str_comma_list_validator
 
 
 class QuicksightGenerateUrlRequest(CustomBaseModel):
-    library_ids: List[int] = Field(
+    library_uuids: List[UUID] = Field(
         description="The list of libraries to include in the dataset, an empty list is equivalent to all the libraries the user is allowed to access."
     )
 
-    @validator("library_ids", pre=True)
-    def parse_library_ids(cls, value):
-        return str_comma_list_validator(value)
+    @validator("library_uuids", pre=True)
+    def parse_library_uuids(cls, value) -> List[UUID]:
+        uuid_list = str_comma_list_validator(value)
+        # verify that all strings in the list are UUIDs
+        return list(map(UUID, uuid_list))
 
 
 class QuicksightGenerateUrlResponse(CustomBaseModel):
