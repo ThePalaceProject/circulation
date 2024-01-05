8938b36c631194479b9340e480cf68faef06a221
Jonathan Green <jonathan@razzard.com>
2024-01-05 15:22:33 -0400

    Test down migrations as well

--- docker/ci/test_migrations.sh
+++ docker/ci/test_migrations.sh
@@ -24,6 +24,37 @@ run_in_container()
   compose_cmd run --build --rm webapp /bin/bash -c "source env/bin/activate && $*"
 }
 
+cleanup() {
+  compose_cmd down
+  git checkout -q "${current_branch}"
+}
+
+run_migrations() {
+  ALEMBIC_CMD=$1
+  run_in_container "alembic ${ALEMBIC_CMD}"
+  exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    echo "ERROR: Running database migrations failed."
+    cleanup
+    exit $exit_code
+  fi
+  echo ""
+}
+
+check_db() {
+  DETAILED_ERROR=$1
+  run_in_container "alembic check"
+  exit_code=$?
+  if [[ $exit_code -eq 0 ]]; then
+    echo "SUCCESS: Database is in sync."
+    echo ""
+  else
+    echo "ERROR: Database is out of sync! ${DETAILED_ERROR}"
+    cleanup
+    exit $exit_code
+  fi
+}
+
 if ! git diff --quiet; then
   echo "ERROR: You have uncommitted changes. These changes will be lost if you run this script."
   echo "  Please commit or stash your changes and try again."
@@ -49,7 +80,7 @@ echo "Current branch: ${current_branch}"
 # Find the first migration file
 first_migration_id=$(run_in_container alembic history -r'base:base+1' -v | head -n 1 | cut -d ' ' -f2)
 if [[ -z $first_migration_id ]]; then
-  echo "ERROR: Could not find first migration."
+  echo "ERROR: Could not find first migration id."
   exit 1
 fi
 
@@ -64,37 +95,40 @@ echo ""
 
 # Find the git commit where the first migration file was added
 first_migration_commit=$(git log --follow --format=%H --reverse "${first_migration_file}" | head -n 1)
+if [[ -z $first_migration_commit ]]; then
+  echo "ERROR: Could not find first migration commit hash."
+  exit 1
+fi
 
 echo "Starting containers and initializing database at commit ${first_migration_commit}"
 git checkout -q "${first_migration_commit}"
 compose_cmd down
 compose_cmd up -d pg
 run_in_container "./bin/util/initialize_instance"
+initialize_exit_code=$?
+if [[ $initialize_exit_code -ne 0 ]]; then
+  echo "ERROR: Failed to initialize instance."
+  cleanup
+  exit $initialize_exit_code
+fi
 echo ""
 
 # Migrate up to the current commit and check if the database is in sync
+echo "Testing upgrade migrations on branch ${current_branch}"
 git checkout -q "${current_branch}"
-echo "Running database migrations on branch ${current_branch}"
-run_in_container "alembic upgrade head"
-exit_code=$?
-if [[ $exit_code -ne 0 ]]; then
-  echo "ERROR: Database migration failed."
-  exit $exit_code
-fi
-echo ""
+run_migrations "upgrade head"
+check_db "A new migration is required or a up migration is broken."
 
-echo "Checking database status"
-run_in_container "alembic check"
-exit_code=$?
-echo ""
+# Migrate down to the first migration and check if the database is in sync
+echo "Testing downgrade migrations"
+run_migrations "downgrade ${first_migration_id}"
+git checkout -q "${first_migration_commit}"
+check_db "A down migration is broken."
 
-if [[ $exit_code -eq 0 ]]; then
-  echo "SUCCESS: Database is in sync."
-else
-  echo "ERROR: Database is out of sync. A new migration is required."
-fi
+# Migrate back up once more to make sure that the database is still in sync
+echo "Testing upgrade migrations a second time"
+git checkout -q "${current_branch}"
+run_migrations "upgrade head"
+check_db "A up migration is likely broken."
 
-# Stop containers
-compose_cmd down
-
-exit $exit_code
+cleanup
