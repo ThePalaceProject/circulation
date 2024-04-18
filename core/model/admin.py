df61db9f51fa598f87f2545cbf59467920f58ece
Jonathan Green <jonathan@razzard.com>
2024-04-18 14:49:26 -0300

    Fix type hint

--- core/model/admin.py
+++ core/model/admin.py
@@ -230,7 +230,7 @@ class Admin(Base, HasSessionCache):
     def generate_reset_password_token(self, secret_key: str) -> str:
         serializer = URLSafeTimedSerializer(secret_key)
 
-        return serializer.dumps(self.email, salt=self.password_hashed)  # type: ignore[return-value]
+        return serializer.dumps(self.email, salt=self.password_hashed)
 
     @staticmethod
     def validate_reset_password_token_and_fetch_admin(
