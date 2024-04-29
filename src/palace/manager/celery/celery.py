efb5ec9613dc3293cf438655af0d423ccb981b72
Jonathan Green <jonathan@razzard.com>
2024-04-29 10:38:25 -0300

    Fix mypy issue

--- src/palace/manager/celery/celery.py
+++ src/palace/manager/celery/celery.py
@@ -2,7 +2,7 @@ import celery
 
 
 class Celery(celery.Celery):
-    def gen_task_name(self, name: str, module: str):
+    def gen_task_name(self, name: str, module: str) -> str:
         """
         This method is used to generate the task name for the Celery task.
 
@@ -13,4 +13,4 @@ class Celery(celery.Celery):
         See: https://docs.celeryq.dev/en/stable/userguide/tasks.html#changing-the-automatic-naming-behavior
         """
         module = module.removeprefix("palace.manager.celery.tasks.")
-        return super().gen_task_name(name, module)
+        return super().gen_task_name(name, module)  # type: ignore[no-any-return]
