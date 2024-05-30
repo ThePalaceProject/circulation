45298a2f705a3967c60c5579110918db6612ef4a
Jonathan Green <jonathan@razzard.com>
2024-05-30 13:41:19 -0300

    Revert customlists to run from a script rather then a celery task (PP-1279) (#1876)
    * Roll back custom list celery job

--- src/palace/manager/scripts/customlist.py
+++ src/palace/manager/scripts/customlist.py
@@ -1,3 +1,7 @@
+import datetime
+import json
+
+from palace.manager.core.query.customlist import CustomListQueries
 from palace.manager.scripts.base import Script
 from palace.manager.scripts.input import LibraryInputScript
 from palace.manager.sqlalchemy.model.customlist import CustomList
@@ -52,3 +56,71 @@ class CustomListManagementScript(Script):
 class UpdateCustomListSizeScript(CustomListSweeperScript):
     def process_custom_list(self, custom_list):
         custom_list.update_size(self._db)
+
+
+class CustomListUpdateEntriesScript(CustomListSweeperScript):
+    """Traverse all entries and update lists if they have auto_update_enabled"""
+
+    def process_custom_list(self, custom_list: CustomList):
+        if not custom_list.auto_update_enabled:
+            return
+        try:
+            self.log.info(f"Auto updating list entries for: {custom_list.name}")
+            self._update_list_with_new_entries(custom_list)
+        except Exception:
+            self.log.exception(f"Could not auto update {custom_list.name}")
+
+    def _update_list_with_new_entries(self, custom_list: CustomList):
+        """Run a search on a custom list, assuming we have auto_update_enabled with a valid query
+        Only json type queries are supported right now, without any support for additional facets
+        """
+
+        start_page = 1
+        json_query = None
+        if custom_list.auto_update_status == CustomList.INIT:
+            # We're in the init phase, we need to back-populate all titles
+            # starting from page 2, since page 1 should be already populated
+            start_page = 2
+        elif custom_list.auto_update_status == CustomList.REPOPULATE:
+            # During a repopulate phase we must empty the list
+            # and start population from page 1
+            for entry in custom_list.entries:
+                self._db.delete(entry)
+            custom_list.entries = []
+        else:
+            # Otherwise we are in an update type process, which means we only search for
+            # "newer" books from the last time we updated the list
+            try:
+                if custom_list.auto_update_query:
+                    json_query = json.loads(custom_list.auto_update_query)
+                else:
+                    return
+            except json.JSONDecodeError as e:
+                self.log.error(
+                    f"Could not decode custom list({custom_list.id}) saved query: {e}"
+                )
+                return
+            # Update availability time as a query part that allows us to filter for new licenses
+            # Although the last_update should never be null, we're failsafing
+            availability_time = (
+                custom_list.auto_update_last_update or datetime.datetime.now()
+            )
+            query_part = json_query["query"]
+            query_part = {
+                "and": [
+                    {
+                        "key": "licensepools.availability_time",
+                        "op": "gte",
+                        "value": availability_time.timestamp(),
+                    },
+                    query_part,
+                ]
+            }
+            # Update the query as such
+            json_query["query"] = query_part
+
+        search = self.services.search.index()
+        CustomListQueries.populate_query_pages(
+            self._db, search, custom_list, json_query=json_query, start_page=start_page
+        )
+        custom_list.auto_update_status = CustomList.UPDATED
