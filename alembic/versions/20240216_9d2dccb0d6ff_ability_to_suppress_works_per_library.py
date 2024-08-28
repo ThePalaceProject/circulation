26d83a632545e5687c0c4c2d471466db60143716
Jonathan Green <jonathan@razzard.com>
2024-08-28 05:16:23 -0300

    Prune old migrations (#2008)

--- alembic/versions/20240216_9d2dccb0d6ff_ability_to_suppress_works_per_library.py
+++ alembic/versions/20240216_9d2dccb0d6ff_ability_to_suppress_works_per_library.py
@@ -4,13 +4,17 @@ Revision ID: 9d2dccb0d6ff
 Revises: 1c9f519415b5
 Create Date: 2024-02-16 17:08:52.146860+00:00
 
+Note: this migration was updated when older migrations were deleted from the repository history,
+and this was made the first migration by changing the down_revision to None.
+
+See: https://alembic.sqlalchemy.org/en/latest/cookbook.html#building-an-up-to-date-database-from-scratch
 """
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "9d2dccb0d6ff"
-down_revision = "1c9f519415b5"
+down_revision = None
 branch_labels = None
 depends_on = None
 
