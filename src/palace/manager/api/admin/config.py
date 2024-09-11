4cda331eb8525dfe573f56718aa203ebba3db7b5
Tim DiLauro <tdilauro@users.noreply.github.com>
2024-09-11 07:42:09 -0400

    Use admin UI v1.23.0 (#2051)

--- src/palace/manager/api/admin/config.py
+++ src/palace/manager/api/admin/config.py
@@ -62,7 +62,7 @@ class OperationalMode(str, Enum):
 class Configuration(LoggerMixin):
     APP_NAME = "Palace Collection Manager"
     PACKAGE_NAME = "@thepalaceproject/circulation-admin"
-    PACKAGE_VERSION = "1.22.0"
+    PACKAGE_VERSION = "1.23.0"
 
     STATIC_ASSETS = {
         "admin_js": "circulation-admin.js",
