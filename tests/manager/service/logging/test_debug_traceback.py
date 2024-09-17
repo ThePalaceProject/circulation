0c59101ea2a27651107e606926a09db21f04dfc1
Jonathan Green <jonathan@razzard.com>
2024-09-17 10:36:07 -0300

    Add a configuration to enable a debug faulthandler (PP-1717) (#2065)
    * Add a configuration to enable a debug faulthandler.
    
    * Fix None handling

---
+++ tests/manager/service/logging/test_debug_traceback.py
@@ -0,0 +1,19 @@
+import faulthandler
+from unittest.mock import patch
+
+from palace.manager.service.logging.debug_traceback import setup_debug_traceback
+
+
+def test_setup_debug_traceback():
+    with patch.object(
+        faulthandler, "dump_traceback_later"
+    ) as mock_dump_traceback_later:
+        # If the interval is 0 or None, the feature should be disabled.
+        setup_debug_traceback(0)
+        mock_dump_traceback_later.assert_not_called()
+        setup_debug_traceback(None)
+        mock_dump_traceback_later.assert_not_called()
+
+        # Otherwise, we should call dump_traceback_later with the correct arguments.
+        setup_debug_traceback(5)
+        mock_dump_traceback_later.assert_called_once_with(5, repeat=True)
