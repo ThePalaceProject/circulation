f351546fa82202c83ea3434711f0c933b457849a
Jonathan Green <jonathan@razzard.com>
2024-10-02 13:20:00 -0300

    Remove unused Overdrive credentials from the database (#2061)
    * Remove unused Overdrive credentials from the database
    
    * Pre-commit fixes

---
+++ alembic/versions/20240916_87901a6323d6_remove_unused_overdrive_credentials.py
@@ -0,0 +1,28 @@
+"""Remove unused Overdrive credentials
+
+Revision ID: 87901a6323d6
+Revises: 350a29bf0ff0
+Create Date: 2024-09-16 19:54:56.986491+00:00
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "87901a6323d6"
+down_revision = "350a29bf0ff0"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Clean out the old unused credential objects, so they are not hanging around and causing confusion
+    # in the future.
+    op.execute(
+        "DELETE from credentials WHERE type is NULL and patron_id is NULL and data_source_id in "
+        "(SELECT id from datasources where name = 'Overdrive')"
+    )
+
+
+def downgrade() -> None:
+    pass
