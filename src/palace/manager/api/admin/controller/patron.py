0ef5e71ddc93fe3dcc1c906b290005a27215f52b
Tim DiLauro <tdilauro@users.noreply.github.com>
2024-11-08 17:54:03 -0500

    Clean up some types.

--- src/palace/manager/api/admin/controller/patron.py
+++ src/palace/manager/api/admin/controller/patron.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+from typing import Any
+
 import flask
 from flask import Response
 from flask_babel import lazy_gettext as _
 
 from palace.manager.api.admin.controller.base import AdminPermissionsControllerMixin
+from palace.manager.api.admin.controller.util import required_library_from_request
 from palace.manager.api.admin.problem_details import NO_SUCH_PATRON
 from palace.manager.api.adobe_vendor_id import AuthdataUtility
 from palace.manager.api.authentication.base import CannotCreateLocalPatron, PatronData
@@ -16,7 +19,9 @@ from palace.manager.util.problem_detail import ProblemDetail
 
 
 class PatronController(CirculationManagerController, AdminPermissionsControllerMixin):
-    def _load_patrondata(self, authenticator=None):
+    def _load_patron_data(
+        self, authenticator: LibraryAuthenticator | None = None
+    ) -> PatronData | ProblemDetail:
         """Extract a patron identifier from an incoming form submission,
         and ask the library's LibraryAuthenticator to turn it into a
         PatronData by doing a remote lookup in the ILS.
@@ -24,19 +29,17 @@ class PatronController(CirculationManagerController, AdminPermissionsControllerM
         :param authenticator: A LibraryAuthenticator. This is for mocking
         during tests; it's not necessary to provide it normally.
         """
-        self.require_librarian(flask.request.library)
+        library = required_library_from_request(flask.request)
+        self.require_librarian(library)
 
         identifier = flask.request.form.get("identifier")
         if not identifier:
             return NO_SUCH_PATRON.detailed(_("Please enter a patron identifier"))
 
         if not authenticator:
-            authenticator = LibraryAuthenticator.from_config(
-                self._db, flask.request.library
-            )
+            authenticator = LibraryAuthenticator.from_config(self._db, library)
 
         patron_data = PatronData(authorization_identifier=identifier)
-        remote_patron_data = None
         patron_lookup_providers = list(authenticator.unique_patron_lookup_providers)
 
         if not patron_lookup_providers:
@@ -45,50 +48,53 @@ class PatronController(CirculationManagerController, AdminPermissionsControllerM
             )
 
         for provider in patron_lookup_providers:
-            remote_patron_data = provider.remote_patron_lookup(patron_data)
-            if remote_patron_data:
-                return remote_patron_data
+            if lookup := getattr(provider, "remote_patron_lookup", None):
+                remote_patron_data: PatronData | None = lookup(patron_data)
+                if remote_patron_data:
+                    return remote_patron_data
 
         # If we get here, none of the providers succeeded.
-        if not remote_patron_data:
-            return NO_SUCH_PATRON.detailed(
-                _(
-                    "No patron with identifier %(patron_identifier)s was found at your library",
-                    patron_identifier=identifier,
-                ),
-            )
+        return NO_SUCH_PATRON.detailed(
+            _(
+                "No patron with identifier %(patron_identifier)s was found at your library",
+                patron_identifier=identifier,
+            ),
+        )
 
-    def lookup_patron(self, authenticator=None):
+    def lookup_patron(
+        self, authenticator: LibraryAuthenticator | None = None
+    ) -> dict[str, Any] | ProblemDetail:
         """Look up personal information about a patron via the ILS.
 
         :param authenticator: A LibraryAuthenticator. This is for mocking
             during tests; it's not necessary to provide it normally.
         """
-        patrondata = self._load_patrondata(authenticator)
-        if isinstance(patrondata, ProblemDetail):
-            return patrondata
-        return patrondata.to_dict
+        patron_data: PatronData | ProblemDetail = self._load_patron_data(authenticator)
+        if isinstance(patron_data, ProblemDetail):
+            return patron_data
+        return patron_data.to_dict
 
-    def reset_adobe_id(self, authenticator=None):
+    def reset_adobe_id(
+        self, authenticator: LibraryAuthenticator | None = None
+    ) -> Response | ProblemDetail:
         """Delete all Credentials for a patron that are relevant
         to the patron's Adobe Account ID.
 
         :param authenticator: A LibraryAuthenticator. This is for mocking
             during tests; it's not necessary to provide it normal
         """
-        patrondata = self._load_patrondata(authenticator)
-        if isinstance(patrondata, ProblemDetail):
-            return patrondata
+        library = required_library_from_request(flask.request)
+        patron_data = self._load_patron_data(authenticator)
+        if isinstance(patron_data, ProblemDetail):
+            return patron_data
         # Turn the Identifier into a Patron object.
         try:
-            patron, is_new = patrondata.get_or_create_patron(
-                self._db, flask.request.library.id
-            )
-        except CannotCreateLocalPatron as e:
+            patron, is_new = patron_data.get_or_create_patron(self._db, library.id)
+        except CannotCreateLocalPatron:
             return NO_SUCH_PATRON.detailed(
                 _(
                     "Could not create local patron object for %(patron_identifier)s",
-                    patron_identifier=patrondata.authorization_identifier,
+                    patron_identifier=patron_data.authorization_identifier,
                 )
             )
 
@@ -98,7 +104,7 @@ class PatronController(CirculationManagerController, AdminPermissionsControllerM
         if patron.username:
             identifier = patron.username
         else:
-            identifier = "with identifier " + patron.authorization_identifier
+            identifier = f"with identifier {patron.authorization_identifier}"
         return Response(
             str(
                 _(
