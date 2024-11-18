4c92f11d95e0aee95e7937796a133325b6a02d5a
Daniel Bernstein <dabylon@gmail.com>
2024-11-18 12:37:19 -0800

    Fix tests and mypy

--- tests/manager/celery/tasks/test_opds_odl.py
+++ tests/manager/celery/tasks/test_opds_odl.py
@@ -3,7 +3,6 @@ from typing import cast
 from unittest.mock import call, patch
 
 import pytest
-from fixtures.services import ServicesFixture
 from freezegun import freeze_time
 from sqlalchemy import func, select
 
@@ -29,6 +28,7 @@ from palace.manager.util.datetime_helpers import utc_now
 from tests.fixtures.celery import CeleryFixture
 from tests.fixtures.database import DatabaseTransactionFixture
 from tests.fixtures.redis import RedisFixture
+from tests.fixtures.services import ServicesFixture
 
 
 class OpdsTaskFixture:
