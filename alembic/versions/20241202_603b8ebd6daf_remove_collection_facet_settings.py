c94cfb01799f1d2427c92bb8f9fc24e53cf9a9b3
dbernstein <dabylon@gmail.com>
2024-12-12 10:07:56 -0800

    Add migration to remove obsolete settings (facets_enabled_collection … (#2204)

---
+++ alembic/versions/20241202_603b8ebd6daf_remove_collection_facet_settings.py
@@ -0,0 +1,27 @@
+"""Remove collection facet settings
+
+Revision ID: 603b8ebd6daf
+Revises: 8dde64eab209
+Create Date: 2024-12-02 19:42:23.775579+00:00
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "603b8ebd6daf"
+down_revision = "8dde64eab209"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    conn = op.get_bind()
+    conn.execute(
+        "UPDATE libraries set settings_dict = "
+        "settings_dict - array['facets_enabled_collection', 'facets_default_collection']"
+    )
+
+
+def downgrade() -> None:
+    pass
