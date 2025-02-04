786d313f2df1233210b5541b7cbc50b6ec8fe813
Jonathan Green <jonathan@razzard.com>
2025-02-04 17:10:46 -0400

    Update some configuration to try to prevent celery task hangs

--- src/palace/manager/service/redis/configuration.py
+++ src/palace/manager/service/redis/configuration.py
@@ -10,3 +10,6 @@ class RedisConfiguration(ServiceConfiguration):
     url: RedisDsn
     key_prefix: str = "palace"
     model_config = SettingsConfigDict(env_prefix="PALACE_REDIS_")
+
+    socket_timeout: float = 15.0
+    socket_connect_timeout: float = 5.0
