c89a6fab7cc9090c2962e0965484a91fc2574dcf
Daniel Bernstein <dabylon@gmail.com>
2025-02-25 09:42:30 -0800

    [PP-2169] Add support for PALACE_CELERY_RESULT_BACKEND_TRANSPORT_OPTIONS_GLOBAL_KEYPREFIX environment variable.

--- tests/manager/service/celery/test_configuration.py
+++ tests/manager/service/celery/test_configuration.py
@@ -28,13 +28,19 @@ class TestCeleryConfiguration:
             "PALACE_CELERY_BROKER_TRANSPORT_OPTIONS_QUEUE_ORDER_STRATEGY", "y"
         )
 
+        monkeypatch.setenv(
+            "PALACE_CELERY_RESULT_BACKEND_TRANSPORT_OPTIONS_GLOBAL_KEYPREFIX", "z"
+        )
+
         config = celery_configuration()
         result = config.model_dump(merge_options=False)
         assert "broker_url" in result
         assert "result_backend" in result
         assert result.get("broker_transport_options_global_keyprefix") == "x"
         assert result.get("broker_transport_options_queue_order_strategy") == "y"
+        assert result.get("result_backend_transport_options_global_keyprefix") == "z"
         assert "broker_transport_options" not in result
+        assert "result_backend_transport_options" not in result
 
     def test_dict_merge(
         self, celery_configuration: CeleryConfFixture, monkeypatch: pytest.MonkeyPatch
@@ -44,6 +50,10 @@ class TestCeleryConfiguration:
         )
         monkeypatch.setenv(
             "PALACE_CELERY_BROKER_TRANSPORT_OPTIONS_QUEUE_ORDER_STRATEGY", "y"
+        )
+
+        monkeypatch.setenv(
+            "PALACE_CELERY_RESULT_BACKEND_TRANSPORT_OPTIONS_GLOBAL_KEYPREFIX", "z"
         )
 
         config = celery_configuration()
@@ -56,3 +66,7 @@ class TestCeleryConfiguration:
         assert options.get("queue_order_strategy") == "y"
         assert "broker_transport_options_global_keyprefix" not in result
         assert "broker_transport_options_queue_order_strategy" not in result
+
+        options = result["result_backend_transport_options"]
+        assert options.get("global_keyprefix") == "z"
+        assert "result_backend_transport_options_global_keyprefix" not in result
