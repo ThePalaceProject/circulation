54f7c1a50b0e3b3ea22114632fa3e5ce0e53178b
Daniel Bernstein <dabylon@gmail.com>
2025-02-25 07:58:50 -0800

    Use Jonathan's enum update helper function.

--- alembic/versions/20250224_704dd5322783_add_task_type_to_timestamp_type_enum.py
+++ alembic/versions/20250224_704dd5322783_add_task_type_to_timestamp_type_enum.py
@@ -6,10 +6,10 @@ Create Date: 2025-02-24 18:24:45.709550+00:00
 
 """
 
-import sqlalchemy
 from alembic import op
 
 from palace.manager.sqlalchemy.model.coverage import Timestamp
+from palace.manager.util.migration.helpers import pg_update_enum
 
 # revision identifiers, used by Alembic.
 revision = "704dd5322783"
@@ -24,50 +24,22 @@ old_options = (
 )
 new_options = old_options + (Timestamp.TASK_TYPE,)
 
-old_type = sqlalchemy.Enum(*old_options, name="service_type")
-new_type = sqlalchemy.Enum(*new_options, name="service_type")
-tmp_type = sqlalchemy.Enum(*new_options, name="_service_type")
-
-tcr = sqlalchemy.sql.table(
-    "timestamps", sqlalchemy.Column("service_type", new_type, nullable=False)
-)
-
 
 def upgrade():
-    # Create a tempoary "service_type" type, convert and drop the "old" type
-    tmp_type.create(op.get_bind(), checkfirst=False)
-    op.execute(
-        "ALTER TABLE timestamps ALTER COLUMN service_type TYPE _service_type"
-        " USING service_type::text::_service_type"
+    # Add task type to the enum
+    pg_update_enum(
+        op, "timestamps", "service_type", "service_type", old_options, new_options
     )
-    old_type.drop(op.get_bind(), checkfirst=False)
-    # Create and convert to the "new" service_type
-    new_type.create(op.get_bind(), checkfirst=False)
-    op.execute(
-        "ALTER TABLE timestamps ALTER COLUMN service_type TYPE service_type"
-        " USING service_type::text::service_type"
-    )
-    tmp_type.drop(op.get_bind(), checkfirst=False)
 
 
 def downgrade():
     # Convert 'task' service_type into 'script'
     op.execute(
-        tcr.update()
-        .where(tcr.c.service_type == "task")
-        .values(service_type=Timestamp.SCRIPT_TYPE)
+        f"UPDATE timestamps SET service_type='{Timestamp.SCRIPT_TYPE}' "
+        f"where service_type='{Timestamp.TASK_TYPE}'"
     )
-    # Create a temporary "_service_type" type, convert and drop the "new" type
-    tmp_type.create(op.get_bind(), checkfirst=False)
-    op.execute(
-        "ALTER TABLE timestamps ALTER COLUMN service_type TYPE _service_type"
-        " USING service_type::text::_service_type"
+
+    # Update the enum
+    pg_update_enum(
+        op, "timestamps", "service_type", "service_type", new_options, old_options
     )
-    new_type.drop(op.get_bind(), checkfirst=False)
-    # Create and convert to the "old" service_type type
-    old_type.create(op.get_bind(), checkfirst=False)
-    op.execute(
-        "ALTER TABLE timestamps ALTER COLUMN service_type TYPE service_type"
-        " USING service_type::text::service_type"
-    )
-    tmp_type.drop(op.get_bind(), checkfirst=False)
