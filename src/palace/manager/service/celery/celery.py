d74e5cbc32981970c566bd0479f9eb0b72ae899a
dbernstein <dabylon@gmail.com>
2025-04-04 11:19:51 -0700

    Adjust delay between launching of axis.reap_collection tasks to 20 se… (#2392)
    …conds
    
    Fire reap_all_collections on Saturday nights.
    
    ## Description
    Celery default queues are filling up again because axis.reap_collection
    tasks are being scheduled past the redis visibility timeout of 1 hour.
    This PR ensures that all tasks will be scheduled well within the one
    hour window. It also changes the weekly schedule to Saturday mornings so
    that I can observe the behavior over the weekend to make sure things are
    fixed.
    
     <!--- Describe your changes -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Checklist
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] All new and existing tests passed.

--- src/palace/manager/service/celery/celery.py
+++ src/palace/manager/service/celery/celery.py
@@ -88,10 +88,10 @@ def beat_schedule() -> dict[str, Any]:
         "axis_reap_all_collections": {
             "task": "axis.reap_all_collections",
             "schedule": crontab(
-                day_of_week="5",
+                day_of_week="6",
                 minute="0",
                 hour="4",
-            ),  # Every Friday at 4:00 AM
+            ),  # Every Saturday at 4:00 AM
         },
         "credential_reaper": {
             "task": "reaper.credential_reaper",
