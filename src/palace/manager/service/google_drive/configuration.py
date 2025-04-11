f1cdc0b6aedb5c96cffef7f70cfb50957d5d4a7d
Daniel Bernstein <dabylon@gmail.com>
2025-04-11 13:00:14 -0700

    Incorporate Jonathan's changes to the google drive config.

--- src/palace/manager/service/google_drive/configuration.py
+++ src/palace/manager/service/google_drive/configuration.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 
 from pydantic import field_validator
 from pydantic_settings import SettingsConfigDict
@@ -18,19 +19,23 @@ PALACE_GOOGLE_DRIVE_PARENT_FOLDER_ID_ENVIRONMENT_VARIABLE = (
 
 class GoogleDriveConfiguration(ServiceConfiguration):
 
-    service_account_info_json: str | None = None
+    # Environment variable specifying the google drive parent folder ID for this environment.
+    # This value may be a folder ID or a shared drive ID. For example, the google drive folder URI will look like
+    # https://drive.google.com/drive/u/1/folders/0AGtlKYStJaC3Uk9PVZ .   "0AGtlKYStJaC3Uk9PVZ" (not a real
+    # folder ID) is the value that should be assigned environment variable.
+    parent_folder_id: str | None = None
+    service_account_info_json: dict[str, Any] | None = None
 
-    @field_validator("service_account_info_json")
+    @field_validator("service_account_info_json", mode="before")
     @classmethod
-    def validate_service_account_info_json(cls, v: str | None) -> str | None:
+    def validate_service_account_info_json(cls, v: Any) -> Any:
         # No validation if service_account_info_json is not provided.
-        if v is None:
-            return None
-
-        try:
-            json.loads(v)
-        except:
-            raise ValueError(f"Unable to parse service_account_info_json: {v}")
+        if isinstance(v, (str, bytes)):
+            # If it's a string, try to parse it as JSON
+            try:
+                return json.loads(v)
+            except:
+                raise ValueError(f"Unable to parse service_account_info_json: {v!r}")
         return v
 
     model_config = SettingsConfigDict(env_prefix="PALACE_GOOGLE_DRIVE_")
--- tests/manager/service/google_drive/test_configuration.py
+++ tests/manager/service/google_drive/test_configuration.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from palace.manager.core.config import CannotLoadConfiguration
@@ -5,18 +7,26 @@ from palace.manager.service.google_drive.configuration import GoogleDriveConfigu
 
 
 def test_invalid_service_account_path_validation_fail():
-    with pytest.raises(CannotLoadConfiguration) as exc_info:
+    with pytest.raises(
+        CannotLoadConfiguration,
+        match=(
+            "PALACE_GOOGLE_DRIVE_SERVICE_ACCOUNT_INFO_JSON:  Value error, "
+            "Unable to parse service_account_info_json: 'blah'"
+        ),
+    ):
         GoogleDriveConfiguration(service_account_info_json="blah")
-
-    assert (
-        "PALACE_GOOGLE_DRIVE_SERVICE_ACCOUNT_INFO_JSON:  Value error, "
-        "Unable to parse service_account_info_json: blah" in str(exc_info.value)
-    )
 
 
 def test_service_account_path_validation_success():
-    service_account_info_json = "{}"
+    service_account_info_json = {"abc": "123"}
+
+    # Can load from a json string
+    configuration = GoogleDriveConfiguration(
+        service_account_info_json=json.dumps(service_account_info_json)
+    )
+    assert configuration.service_account_info_json == service_account_info_json
+
+    # Or from a dict
     configuration = GoogleDriveConfiguration(
         service_account_info_json=service_account_info_json
     )
-    assert configuration.service_account_info_json == service_account_info_json
