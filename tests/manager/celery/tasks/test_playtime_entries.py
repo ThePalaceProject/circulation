94c8191943143e01a6cfabfd7e320fe078db9541
Daniel Bernstein <dabylon@gmail.com>
2025-04-15 08:20:16 -0700

    Move script tests into a test file in the tests/manager/scripts directory.

--- tests/manager/celery/tasks/test_playtime_entries.py
+++ tests/manager/celery/tasks/test_playtime_entries.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import csv
-import re
 from datetime import date, datetime, timedelta
 from io import IOBase, StringIO
 from typing import Any, Literal
@@ -9,7 +8,6 @@ from unittest.mock import DEFAULT, create_autospec
 
 import pytest
 import pytz
-from freezegun import freeze_time
 from sqlalchemy.sql.expression import and_, null
 
 from palace.manager.api.model.time_tracking import PlaytimeTimeEntry
@@ -22,16 +20,13 @@ from palace.manager.core.config import Configuration
 from palace.manager.core.equivalents_coverage import (
     EquivalentIdentifiersCoverageProvider,
 )
-from palace.manager.scripts.playtime_entries import (
-    PlaytimeEntriesReportsScript,
-)
 from palace.manager.service.google_drive.google_drive import GoogleDriveService
 from palace.manager.sqlalchemy.model.collection import Collection
 from palace.manager.sqlalchemy.model.datasource import DataSource
 from palace.manager.sqlalchemy.model.identifier import Equivalency, Identifier
 from palace.manager.sqlalchemy.model.library import Library
 from palace.manager.sqlalchemy.model.time_tracking import PlaytimeEntry, PlaytimeSummary
-from palace.manager.util.datetime_helpers import datetime_utc, previous_months, utc_now
+from palace.manager.util.datetime_helpers import previous_months, utc_now
 from tests.fixtures.celery import CeleryFixture
 from tests.fixtures.database import DatabaseTransactionFixture
 from tests.fixtures.services import ServicesFixture
@@ -869,148 +864,3 @@ class TestGeneratePlaytimeReport:
                 "2025",
             ],
         }
-
-    @pytest.mark.parametrize(
-        "current_utc_time, start_arg, expected_start, until_arg, expected_until",
-        [
-            # Default values from two dates within the same month (next two cases).
-            [
-                datetime(2020, 1, 1, 0, 0, 0),
-                None,
-                datetime_utc(2019, 12, 1, 0, 0, 0),
-                None,
-                datetime_utc(2020, 1, 1, 0, 0, 0),
-            ],
-            [
-                datetime(2020, 1, 31, 0, 0, 0),
-                None,
-                datetime_utc(2019, 12, 1, 0, 0, 0),
-                None,
-                datetime_utc(2020, 1, 1, 0, 0, 0),
-            ],
-            # `start` specified, `until` defaulted.
-            [
-                datetime(2020, 1, 31, 0, 0, 0),
-                "2019-06-11",
-                datetime_utc(2019, 6, 11, 0, 0, 0),
-                None,
-                datetime_utc(2020, 1, 1, 0, 0, 0),
-            ],
-            # `start` defaulted, `until` specified.
-            [
-                datetime(2020, 1, 31, 0, 0, 0),
-                None,
-                datetime_utc(2019, 12, 1, 0, 0, 0),
-                "2019-12-20",
-                datetime_utc(2019, 12, 20, 0, 0, 0),
-            ],
-            # When both dates are specified, the current datetime doesn't matter.
-            # Both dates specified, but we test at a specific time here anyway.
-            [
-                datetime(2020, 1, 31, 0, 0, 0),
-                "2018-07-03",
-                datetime_utc(2018, 7, 3, 0, 0, 0),
-                "2019-04-30",
-                datetime_utc(2019, 4, 30, 0, 0, 0),
-            ],
-            # The same dates are specified, but we test at the actual current time.
-            [
-                utc_now(),
-                "2018-07-03",
-                datetime_utc(2018, 7, 3, 0, 0, 0),
-                "2019-04-30",
-                datetime_utc(2019, 4, 30, 0, 0, 0),
-            ],
-            # The same dates are specified, but we test at the actual current time.
-            [
-                utc_now(),
-                "4099-07-03",
-                datetime_utc(4099, 7, 3, 0, 0, 0),
-                "4150-04-30",
-                datetime_utc(4150, 4, 30, 0, 0, 0),
-            ],
-        ],
-    )
-    def test_parse_command_line(
-        self,
-        current_utc_time: datetime,
-        start_arg: str | None,
-        expected_start: datetime,
-        until_arg: str | None,
-        expected_until: datetime,
-    ):
-        start_args = ["--start", start_arg] if start_arg else []
-        until_args = ["--until", until_arg] if until_arg else []
-        cmd_args = start_args + until_args
-
-        with freeze_time(current_utc_time):
-            parsed = PlaytimeEntriesReportsScript.parse_command_line(cmd_args=cmd_args)
-        assert expected_start == parsed.start
-        assert expected_until == parsed.until
-        assert pytz.UTC == parsed.start.tzinfo
-        assert pytz.UTC == parsed.until.tzinfo
-
-    @pytest.mark.parametrize(
-        "current_utc_time, start_arg, expected_start, until_arg, expected_until",
-        [
-            # `start` specified, `until` defaulted.
-            [
-                datetime(2020, 1, 31, 0, 0, 0),
-                "2020-02-01",
-                datetime_utc(2020, 2, 1, 0, 0, 0),
-                None,
-                datetime_utc(2020, 1, 1, 0, 0, 0),
-            ],
-            # `start` defaulted, `until` specified.
-            [
-                datetime(2020, 1, 31, 0, 0, 0),
-                None,
-                datetime_utc(2019, 12, 1, 0, 0, 0),
-                "2019-06-11",
-                datetime_utc(2019, 6, 11, 0, 0, 0),
-            ],
-            # When both dates are specified, the current datetime doesn't matter.
-            # Both dates specified, but we test at a specific time here anyway.
-            [
-                datetime(2020, 1, 31, 0, 0, 0),
-                "2019-04-30",
-                datetime_utc(2019, 4, 30, 0, 0, 0),
-                "2018-07-03",
-                datetime_utc(2018, 7, 3, 0, 0, 0),
-            ],
-            # The same dates are specified, but we test at the actual current time.
-            [
-                utc_now(),
-                "2019-04-30",
-                datetime_utc(2019, 4, 30, 0, 0, 0),
-                "2018-07-03",
-                datetime_utc(2018, 7, 3, 0, 0, 0),
-            ],
-            # The same dates are specified, but we test at the actual current time.
-            [
-                utc_now(),
-                "4150-04-30",
-                datetime_utc(4150, 4, 30, 0, 0, 0),
-                "4099-07-03",
-                datetime_utc(4099, 7, 3, 0, 0, 0),
-            ],
-        ],
-    )
-    def test_parse_command_line_start_not_before_until(
-        self,
-        capsys,
-        current_utc_time: datetime,
-        start_arg: str | None,
-        expected_start: datetime,
-        until_arg: str | None,
-        expected_until: datetime,
-    ):
-        start_args = ["--start", start_arg] if start_arg else []
-        until_args = ["--until", until_arg] if until_arg else []
-        cmd_args = start_args + until_args
-
-        with freeze_time(current_utc_time), pytest.raises(SystemExit) as excinfo:
-            parsed = PlaytimeEntriesReportsScript.parse_command_line(cmd_args=cmd_args)
-        _, err = capsys.readouterr()
-        assert 2 == excinfo.value.code
-        assert re.search(r"start date \(.*\) must be before until date \(.*\).", err)
---
+++ tests/manager/scripts/test_playtime_entries_scripts.py
@@ -0,0 +1,161 @@
+from __future__ import annotations
+
+import re
+from datetime import datetime
+
+import pytest
+import pytz
+from freezegun import freeze_time
+
+from palace.manager.scripts.playtime_entries import (
+    PlaytimeEntriesReportsScript,
+)
+from palace.manager.util.datetime_helpers import datetime_utc, utc_now
+
+
+class TestPlaytimeEntriesReportScript:
+
+    @pytest.mark.parametrize(
+        "current_utc_time, start_arg, expected_start, until_arg, expected_until",
+        [
+            # Default values from two dates within the same month (next two cases).
+            [
+                datetime(2020, 1, 1, 0, 0, 0),
+                None,
+                datetime_utc(2019, 12, 1, 0, 0, 0),
+                None,
+                datetime_utc(2020, 1, 1, 0, 0, 0),
+            ],
+            [
+                datetime(2020, 1, 31, 0, 0, 0),
+                None,
+                datetime_utc(2019, 12, 1, 0, 0, 0),
+                None,
+                datetime_utc(2020, 1, 1, 0, 0, 0),
+            ],
+            # `start` specified, `until` defaulted.
+            [
+                datetime(2020, 1, 31, 0, 0, 0),
+                "2019-06-11",
+                datetime_utc(2019, 6, 11, 0, 0, 0),
+                None,
+                datetime_utc(2020, 1, 1, 0, 0, 0),
+            ],
+            # `start` defaulted, `until` specified.
+            [
+                datetime(2020, 1, 31, 0, 0, 0),
+                None,
+                datetime_utc(2019, 12, 1, 0, 0, 0),
+                "2019-12-20",
+                datetime_utc(2019, 12, 20, 0, 0, 0),
+            ],
+            # When both dates are specified, the current datetime doesn't matter.
+            # Both dates specified, but we test at a specific time here anyway.
+            [
+                datetime(2020, 1, 31, 0, 0, 0),
+                "2018-07-03",
+                datetime_utc(2018, 7, 3, 0, 0, 0),
+                "2019-04-30",
+                datetime_utc(2019, 4, 30, 0, 0, 0),
+            ],
+            # The same dates are specified, but we test at the actual current time.
+            [
+                utc_now(),
+                "2018-07-03",
+                datetime_utc(2018, 7, 3, 0, 0, 0),
+                "2019-04-30",
+                datetime_utc(2019, 4, 30, 0, 0, 0),
+            ],
+            # The same dates are specified, but we test at the actual current time.
+            [
+                utc_now(),
+                "4099-07-03",
+                datetime_utc(4099, 7, 3, 0, 0, 0),
+                "4150-04-30",
+                datetime_utc(4150, 4, 30, 0, 0, 0),
+            ],
+        ],
+    )
+    def test_parse_command_line(
+        self,
+        current_utc_time: datetime,
+        start_arg: str | None,
+        expected_start: datetime,
+        until_arg: str | None,
+        expected_until: datetime,
+    ):
+        start_args = ["--start", start_arg] if start_arg else []
+        until_args = ["--until", until_arg] if until_arg else []
+        cmd_args = start_args + until_args
+
+        with freeze_time(current_utc_time):
+            parsed = PlaytimeEntriesReportsScript.parse_command_line(cmd_args=cmd_args)
+        assert expected_start == parsed.start
+        assert expected_until == parsed.until
+        assert pytz.UTC == parsed.start.tzinfo
+        assert pytz.UTC == parsed.until.tzinfo
+
+    @pytest.mark.parametrize(
+        "current_utc_time, start_arg, expected_start, until_arg, expected_until",
+        [
+            # `start` specified, `until` defaulted.
+            [
+                datetime(2020, 1, 31, 0, 0, 0),
+                "2020-02-01",
+                datetime_utc(2020, 2, 1, 0, 0, 0),
+                None,
+                datetime_utc(2020, 1, 1, 0, 0, 0),
+            ],
+            # `start` defaulted, `until` specified.
+            [
+                datetime(2020, 1, 31, 0, 0, 0),
+                None,
+                datetime_utc(2019, 12, 1, 0, 0, 0),
+                "2019-06-11",
+                datetime_utc(2019, 6, 11, 0, 0, 0),
+            ],
+            # When both dates are specified, the current datetime doesn't matter.
+            # Both dates specified, but we test at a specific time here anyway.
+            [
+                datetime(2020, 1, 31, 0, 0, 0),
+                "2019-04-30",
+                datetime_utc(2019, 4, 30, 0, 0, 0),
+                "2018-07-03",
+                datetime_utc(2018, 7, 3, 0, 0, 0),
+            ],
+            # The same dates are specified, but we test at the actual current time.
+            [
+                utc_now(),
+                "2019-04-30",
+                datetime_utc(2019, 4, 30, 0, 0, 0),
+                "2018-07-03",
+                datetime_utc(2018, 7, 3, 0, 0, 0),
+            ],
+            # The same dates are specified, but we test at the actual current time.
+            [
+                utc_now(),
+                "4150-04-30",
+                datetime_utc(4150, 4, 30, 0, 0, 0),
+                "4099-07-03",
+                datetime_utc(4099, 7, 3, 0, 0, 0),
+            ],
+        ],
+    )
+    def test_parse_command_line_start_not_before_until(
+        self,
+        capsys,
+        current_utc_time: datetime,
+        start_arg: str | None,
+        expected_start: datetime,
+        until_arg: str | None,
+        expected_until: datetime,
+    ):
+        start_args = ["--start", start_arg] if start_arg else []
+        until_args = ["--until", until_arg] if until_arg else []
+        cmd_args = start_args + until_args
+
+        with freeze_time(current_utc_time), pytest.raises(SystemExit) as excinfo:
+            parsed = PlaytimeEntriesReportsScript.parse_command_line(cmd_args=cmd_args)
+        _, err = capsys.readouterr()
+        assert 2 == excinfo.value.code
+        assert re.search(r"start date \(.*\) must be before until date \(.*\).", err)
