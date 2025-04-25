1f766a947f6032aa34d63ecaa6824def58e3c041
Daniel Bernstein <dabylon@gmail.com>
2025-04-24 17:18:22 -0700

    Add a couple of tests for script.

--- src/palace/manager/scripts/axis_import.py
+++ src/palace/manager/scripts/axis_import.py
@@ -29,7 +29,11 @@ class ImportCollection(Script):
     def do_run(self, *args, **kwargs):
         parsed = self.parse_command_line(self._db, *args, **kwargs)
         collection_name = parsed.collection_name
+
         collection = Collection.by_name(self._db, collection_name)
+        if not collection:
+            raise ValueError(f'No collection found named "{collection_name}".')
+
         list_identifiers_for_import.apply_async(
             kwargs={"collection_id": collection.id, "import_all": parsed.import_all},
             link=import_identifiers.s(
