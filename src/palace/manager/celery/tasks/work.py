ca9d9d8e934e6a2e6024398be2b301f806d1f60f
Daniel Bernstein <dabylon@gmail.com>
2025-06-10 10:59:03 -0700

    Add periodic task for workcoveragerecord migration to redis set.

--- src/palace/manager/celery/tasks/work.py
+++ src/palace/manager/celery/tasks/work.py
@@ -1,11 +1,14 @@
 from celery import shared_task
+from sqlalchemy import delete, select
 
 from palace.manager.celery.task import Task
+from palace.manager.data_layer.policy.presentation import PresentationCalculationPolicy
 from palace.manager.service.celery.celery import QueueNames
 from palace.manager.service.redis.models.lock import TaskLock
 from palace.manager.service.redis.models.work import (
     WaitingForPresentationCalculation,
 )
+from palace.manager.sqlalchemy.model.coverage import WorkCoverageRecord
 from palace.manager.sqlalchemy.model.work import Work
 from palace.manager.sqlalchemy.util import get_one
 from palace.manager.util.log import elapsed_time_logging
@@ -52,3 +55,31 @@ def calculate_work_presentations(
         raise task.replace(calculate_work_presentations.s(batch_size=batch_size))
 
     task.log.info(f"Finished calculating presentation for works.")
+
+
+@shared_task(queue=QueueNames.default, bind=True)
+def migrate_work_coverage_records(task: Task) -> None:
+    """
+    TODO: Remove in next release
+    This task is TEMPORARY and should be removed in the next release along with the reference to it in
+    palace/manager/service/celery/celery.py
+    Initially I had implemented this routine as an alembic migration but caused issues with the tests
+    and introduced unnecessary complexity into the migration process.
+    """
+    with task.transaction() as session:
+        rows = session.execute(
+            select(WorkCoverageRecord.work_id).where(
+                WorkCoverageRecord.status == WorkCoverageRecord.REGISTERED
+            )
+        ).all()
+
+        policy = PresentationCalculationPolicy.recalculate_everything()
+
+        for row in rows:
+            Work.queue_presentation_recalculation(work_id=row["work_id"], policy=policy)
+
+        session.execute(
+            delete(WorkCoverageRecord.work_id).where(
+                WorkCoverageRecord.status == WorkCoverageRecord.REGISTERED
+            )
+        )
