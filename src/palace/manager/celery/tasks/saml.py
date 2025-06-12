f87eed23e1eacb7150cdb8df9588b61a99122c8f
dbernstein <dabylon@gmail.com>
2025-06-12 11:49:59 -0700

    [PP-1479] convert saml scripts to celery (#2502)
    ## Description
    This PR converts the saml_monitor script to a celery task. It runs on
    the same schedule (ie once a day at 5am). It can be manually triggered
    using the ./bin/update_saml_metadata script.
    
    ## Motivation and Context
    This task is part of the larger effort to convert our scripts to use the
    celery task processing framework.
    https://ebce-lyrasis.atlassian.net/browse/PP-1479
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How Has This Been Tested?
    New unit tests cover the new functionality.
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Checklist
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] I have updated the documentation accordingly.
    - [x] All new and existing tests passed.

---
+++ src/palace/manager/celery/tasks/saml.py
@@ -0,0 +1,83 @@
+from logging import Logger
+
+from celery import shared_task
+from sqlalchemy.orm import Session
+
+from palace.manager.api.saml.metadata.federations.loader import (
+    SAMLFederatedIdentityProviderLoader,
+    SAMLMetadataLoader,
+)
+from palace.manager.api.saml.metadata.federations.validator import (
+    SAMLFederatedMetadataExpirationValidator,
+    SAMLFederatedMetadataValidatorChain,
+    SAMLMetadataSignatureValidator,
+)
+from palace.manager.api.saml.metadata.parser import SAMLMetadataParser
+from palace.manager.celery.task import Task
+from palace.manager.service.celery.celery import QueueNames
+from palace.manager.sqlalchemy.model.saml import SAMLFederation
+from palace.manager.util.datetime_helpers import utc_now
+from palace.manager.util.log import LoggerAdapterType
+
+
+def _update_saml_federation_idps_metadata(
+    saml_federation: SAMLFederation,
+    loader: SAMLFederatedIdentityProviderLoader,
+    session: Session,
+    log: Logger | LoggerAdapterType,
+) -> None:
+    """Update IdPs' metadata belonging to the specified SAML federation."""
+    log.info(f"Started processing {saml_federation}")
+
+    for existing_identity_provider in saml_federation.identity_providers:
+        session.delete(existing_identity_provider)
+
+    new_identity_providers = loader.load(saml_federation)
+
+    for new_identity_provider in new_identity_providers:
+        session.add(new_identity_provider)
+        new_identity_provider
+
+    saml_federation.last_updated_at = utc_now()
+
+    log.info(f"Finished processing {saml_federation}")
+
+
+@shared_task(queue=QueueNames.default, bind=True)
+def update_saml_federation_idps_metadata(task: Task) -> None:
+    """
+    For each SAML Fedoration in the CM, update the federations IdPs metadata
+    Please note that the monitor looks for federations in the `samlfederations` table.
+    Currently, there is no way to configure SAML federations in the admin interface.
+    """
+
+    saml_federated_idp_loader = _create_saml_federated_identity_provider_loader()
+
+    with task.session() as session:
+        saml_federations = session.query(SAMLFederation).all()
+
+        task.log.info(f"Found {len(saml_federations)} SAML federations")
+
+        for outdated_saml_federation in saml_federations:
+            _update_saml_federation_idps_metadata(
+                outdated_saml_federation,
+                saml_federated_idp_loader,
+                session,
+                task.log,
+            )
+
+    task.log.info("Finished updating the SAML metadata")
+
+
+def _create_saml_federated_identity_provider_loader() -> (
+    SAMLFederatedIdentityProviderLoader
+):
+    saml_metadata_loader = SAMLMetadataLoader()
+    saml_metadata_validator = SAMLFederatedMetadataValidatorChain(
+        [SAMLFederatedMetadataExpirationValidator(), SAMLMetadataSignatureValidator()]
+    )
+    saml_metadata_parser = SAMLMetadataParser(skip_incorrect_providers=True)
+    saml_federated_idp_loader = SAMLFederatedIdentityProviderLoader(
+        saml_metadata_loader, saml_metadata_validator, saml_metadata_parser
+    )
+    return saml_federated_idp_loader
---
+++ tests/manager/celery/tasks/test_saml.py
@@ -0,0 +1,63 @@
+from unittest.mock import MagicMock, create_autospec, patch
+
+from palace.manager.api.saml.metadata.federations.loader import (
+    SAMLFederatedIdentityProviderLoader,
+)
+from palace.manager.celery.tasks.saml import (
+    _create_saml_federated_identity_provider_loader,
+    update_saml_federation_idps_metadata,
+)
+from palace.manager.sqlalchemy.model.saml import (
+    SAMLFederatedIdentityProvider,
+    SAMLFederation,
+)
+from tests.fixtures.celery import CeleryFixture
+from tests.fixtures.database import DatabaseTransactionFixture
+from tests.mocks import saml_strings
+
+
+def test_update_saml_federation_idps_metadata_task(
+    db: DatabaseTransactionFixture, celery_fixture: CeleryFixture
+):
+    # Arrange
+    expected_federation = SAMLFederation(
+        "Test federation", "http://incommon.org/metadata"
+    )
+    expected_federated_identity_providers = [
+        SAMLFederatedIdentityProvider(
+            expected_federation,
+            saml_strings.IDP_1_ENTITY_ID,
+            saml_strings.IDP_1_UI_INFO_EN_DISPLAY_NAME,
+            saml_strings.CORRECT_XML_WITH_IDP_1,
+        ),
+        SAMLFederatedIdentityProvider(
+            expected_federation,
+            saml_strings.IDP_2_ENTITY_ID,
+            saml_strings.IDP_2_UI_INFO_EN_DISPLAY_NAME,
+            saml_strings.CORRECT_XML_WITH_IDP_2,
+        ),
+    ]
+
+    db.session.add_all([expected_federation])
+    db.session.add_all(expected_federated_identity_providers)
+
+    loader = create_autospec(spec=SAMLFederatedIdentityProviderLoader)
+    loader.load = MagicMock(return_value=expected_federated_identity_providers)
+
+    with patch(
+        "palace.manager.celery.tasks.saml._create_saml_federated_identity_provider_loader"
+    ) as create_loader:
+        create_loader.return_value = loader
+
+        # Invoke
+        update_saml_federation_idps_metadata.delay().wait()
+        # Assert
+        identity_providers: list[SAMLFederatedIdentityProvider] = db.session.query(
+            SAMLFederatedIdentityProvider
+        ).all()
+        assert expected_federated_identity_providers == identity_providers
+
+
+def test_create_saml_federated_identity_provider_loader():
+
+    assert _create_saml_federated_identity_provider_loader()
