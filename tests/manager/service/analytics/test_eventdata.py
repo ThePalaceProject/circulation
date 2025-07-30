0e750df7ad4554555f92d31423d53c620591f7fa
Jonathan Green <jonathan@razzard.com>
2025-07-29 23:17:28 -0300

    Don't log warning

--- tests/manager/service/analytics/test_eventdata.py
+++ tests/manager/service/analytics/test_eventdata.py
@@ -1,4 +1,10 @@
+from unittest.mock import PropertyMock, patch
+
+import pytest
+
+from palace.manager.service.analytics import eventdata
 from palace.manager.service.analytics.eventdata import AnalyticsEventData
+from palace.manager.service.logging.configuration import LogLevel
 from palace.manager.sqlalchemy.model.circulationevent import CirculationEvent
 from tests.fixtures.database import DatabaseTransactionFixture
 from tests.fixtures.flask import FlaskAppFixture
@@ -9,7 +15,10 @@ class TestAnalyticsEventData:
         self,
         db: DatabaseTransactionFixture,
         flask_app_fixture: FlaskAppFixture,
+        caplog: pytest.LogCaptureFixture,
     ) -> None:
+        caplog.set_level(LogLevel.warning)
+
         edition = db.edition()
         pool = db.licensepool(edition=edition)
         library = db.default_library()
@@ -39,6 +48,20 @@ class TestAnalyticsEventData:
             )
         assert event.user_agent is None
 
-        # call outside of request context
+        # call outside of request context, we don't log a warning, as this is an expected case
+        # we just set user_agent to None
         event = AnalyticsEventData.create(library, pool, CirculationEvent.CM_CHECKOUT)
         assert event.user_agent is None
+        assert len(caplog.records) == 0
+
+        # Exception getting user agent. This isn't expected, but isn't fatal, so we log a warning
+        with patch.object(eventdata, "flask") as mock_flask:
+            type(mock_flask.request).user_agent = PropertyMock(
+                side_effect=Exception("Test exception")
+            )
+            event = AnalyticsEventData.create(
+                library, pool, CirculationEvent.CM_CHECKOUT
+            )
+        assert event.user_agent is None
+        assert len(caplog.records) == 1
+        assert "Unable to resolve the user_agent" in caplog.text
