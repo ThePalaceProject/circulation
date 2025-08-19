473eadc724f92ed06f3dd98fa53e1a7d29961cba
dbernstein <dabylon@gmail.com>
2025-08-18 17:54:30 -0700

    More calculate work optimizations (#2665)
    ## Description
    We are still seeing deadlocks and long waits on row level locks for data
    being updated during work.calculate_work_presentation tasks. This update
    attempts to reduce lock contention by preventing unnecessary database
    writes when updating contributors (a source of two different classes of
    deadlocks), updating hyperlinks (a source of one class of deadlock) and
    updating measurements (not a source of dead locks but appears to be
    implicated in high database load due to long lock waits).
    
    ## Motivation and Context
    https://ebce-lyrasis.atlassian.net/browse/PP-2788
    https://ebce-lyrasis.atlassian.net/browse/PP-2763
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How Has This Been Tested?
    Unit tests updated.  
    New tests added:  
    * one test to confirm that updating the contributors in replace mode
    with no data changes results in no deletion and reinsertion.
    * one to confirm both updating the identifier links in replace mode with
    no data changes results in no deletions or insertions.
    * one to confirm that updating the identifier links in replace mode with
    data changes results in insertions of new data and deletions of stale
    data. Unchanged data is not touched.
    
    When I initially updated 
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Checklist
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] I have updated the documentation accordingly.
    - [x] All new and existing tests passed.

--- tests/manager/data_layer/test_bibliographic.py
+++ tests/manager/data_layer/test_bibliographic.py
@@ -291,6 +291,96 @@ class TestBibliographicData:
         assert "http://largeimage.com/" == edition.cover_full_url
         assert None == edition.cover_thumbnail_url
 
+    def test_links_are_are_inserted_and_deleted_when_in_replace_mode_when_change_occurs(
+        self, db: DatabaseTransactionFixture
+    ):
+        edition = db.edition()
+        l1 = LinkData(
+            rel=Hyperlink.IMAGE,
+            href="http://example.com/",
+            media_type=Representation.JPEG_MEDIA_TYPE,
+        )
+
+        link_to_be_deleted, ignore = edition.primary_identifier.add_link(
+            rel=Hyperlink.IMAGE,
+            href="http://example.com/to_be_deleted",
+            data_source=edition.data_source,
+            media_type=Representation.JPEG_MEDIA_TYPE,
+        )
+
+        assert len(edition.primary_identifier.links) == 1
+
+        bibliographic = BibliographicData(
+            links=[l1],
+            data_source_name=edition.data_source.name,
+        )
+        replace = ReplacementPolicy(links=True)
+        bibliographic.apply(db.session, edition, None, replace=replace)
+
+        # we expect a change because the image link has changed.
+        assert len(edition.primary_identifier.links) == 1
+        assert edition.primary_identifier.links != [link_to_be_deleted]
+
+    def test_that_links_are_not_deleted_and_reinserted_when_no_change(
+        self, db: DatabaseTransactionFixture
+    ):
+        edition = db.edition()
+        l1 = LinkData(
+            rel=Hyperlink.IMAGE,
+            href="http://example.com/existing_link",
+            media_type=Representation.JPEG_MEDIA_TYPE,
+        )
+
+        existing_link, ignore = edition.primary_identifier.add_link(
+            rel=Hyperlink.IMAGE,
+            href="http://example.com/existing_link",
+            data_source=edition.data_source,
+            media_type=Representation.JPEG_MEDIA_TYPE,
+        )
+
+        assert len(edition.primary_identifier.links) == 1
+
+        bibliographic = BibliographicData(
+            links=[l1],
+            data_source_name=edition.data_source.name,
+        )
+        replace = ReplacementPolicy(links=True)
+        bibliographic.apply(db.session, edition, None, replace=replace)
+
+        # we expect a change because we're in replace mode and the existing link disappeared and a new one was added.
+        assert len(edition.primary_identifier.links) == 1
+        assert existing_link.id == edition.primary_identifier.links[0].id
+
+    def test_that_new_links_are_inserted_and_existing_links_not_deleted_and_reinserted_when_replacement_policy_is_false(
+        self, db: DatabaseTransactionFixture
+    ):
+        edition = db.edition()
+        l1 = LinkData(
+            rel=Hyperlink.IMAGE,
+            href="http://example.com/new_link",
+            media_type=Representation.JPEG_MEDIA_TYPE,
+        )
+
+        existing_link, ignore = edition.primary_identifier.add_link(
+            rel=Hyperlink.IMAGE,
+            href="http://example.com/existing_link",
+            data_source=edition.data_source,
+            media_type=Representation.JPEG_MEDIA_TYPE,
+        )
+
+        assert len(edition.primary_identifier.links) == 1
+
+        bibliographic = BibliographicData(
+            links=[l1],
+            data_source_name=edition.data_source.name,
+        )
+        replace = ReplacementPolicy(links=False)
+        bibliographic.apply(db.session, edition, None, replace=replace)
+
+        # We expect the new link to be added and the existing link preserved.
+        assert len(edition.primary_identifier.links) == 2
+        assert existing_link in edition.primary_identifier.links
+
     def test_measurements(self, db: DatabaseTransactionFixture):
         edition = db.edition()
         measurement = MeasurementData(
@@ -701,6 +791,37 @@ class TestBibliographicData:
         assert "79096089" == contributor.viaf
         assert "123" == contributor.lc
         assert "Robert_Jordan" == contributor.wikipedia_name
+
+    def test_update_contributions_in_relace_mode_with_no_changes(
+        self, db: DatabaseTransactionFixture
+    ):
+        edition = db.edition()
+        original_contributions = list(edition.contributions)
+        original_contributors = edition.contributors
+        assert len(edition.contributions) == 1
+        contribution = edition.contributions[0]
+        contributor = contribution.contributor
+
+        # update the contributions with the identical contribution.
+        contributor_data = ContributorData(
+            display_name=contributor.display_name,
+            sort_name=contributor.sort_name,
+            wikipedia_name=contributor.wikipedia_name,
+            viaf=contributor.viaf,
+            lc=contributor.lc,
+            roles=[contribution.role],
+        )
+
+        bibliographic = BibliographicData(
+            data_source_name=DataSource.OVERDRIVE, contributors=[contributor_data]
+        )
+
+        # we expect that no change occurred despite the replace flag.
+        assert not bibliographic.update_contributions(db.session, edition, replace=True)
+
+        # validate that the contributions did not change since no change occurred.
+        assert edition.contributions == original_contributions
+        assert edition.contributors == original_contributors
 
     def test_update_contributions_with_blank_display_name(
         self, db: DatabaseTransactionFixture
