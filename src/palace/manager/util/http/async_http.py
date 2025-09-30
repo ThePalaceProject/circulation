cc0978d3ec986daa1f1c92eb1528f26f55e610f2
Daniel Bernstein <dabylon@gmail.com>
2025-09-30 13:01:35 -0700

    Fix mypy

--- src/palace/manager/util/http/async_http.py
+++ src/palace/manager/util/http/async_http.py
@@ -7,6 +7,8 @@ from types import TracebackType
 from typing import IO, Any, Self, TypedDict, Union, Unpack, cast
 
 import httpx
+from httpx import URL
+from typing_extensions import Self, Unpack
 
 from palace.manager.util.backoff import exponential_backoff
 from palace.manager.util.http.base import (
@@ -367,6 +369,14 @@ class AsyncClient(LoggerMixin):
     @property
     def headers(self) -> httpx.Headers:
         return self._httpx_client.headers
+
+    @property
+    def base_url(self) -> URL:
+        return self._httpx_client.base_url
+
+    @base_url.setter
+    def base_url(self, base_url: URL | str) -> None:
+        self._httpx_client.base_url = base_url
 
     async def _perform_request(
         self,
