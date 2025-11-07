d15d73aa835cbf2e468b45dd05928d0e0959829d
Tim DiLauro <tdilauro@users.noreply.github.com>
2025-11-07 09:27:32 -0500

    `update_saml_metadata` saves changes (PP-3216) (#2868)
    ## Description
    
    Use `task.transaction`, rather than `task.session` in SAML federation
    IdP update task, so that the database changes will be committed.
    
    ## Motivation and Context
    
    The `bin/update_saml_metadata` script appeared to run successfully, but
    the results did not make it to the database.
    
    [Jira PP-3216]
    
    ## How Has This Been Tested?
    
    - Updated test to fail before the fix and passes after.
    - All tests pass locally.
    - [CI
    tests](https://github.com/ThePalaceProject/circulation/actions/runs/19145895263)
    pass.
    
    ## Checklist
    
    - [x] I have updated the documentation accordingly.
    - [x] All new and existing tests passed.

--- tests/manager/celery/tasks/test_saml.py
+++ tests/manager/celery/tasks/test_saml.py
@@ -19,43 +19,52 @@ from tests.mocks import saml_strings
 def test_update_saml_federation_idps_metadata_task(
     db: DatabaseTransactionFixture, celery_fixture: CeleryFixture
 ):
-    # Arrange
-    expected_federation = SAMLFederation(
-        "Test federation", "http://incommon.org/metadata"
-    )
-    expected_federated_identity_providers = [
+    # Add a federation to the database and commit it.
+    # This mimics what we currently do when setting up a SAML federation
+    # using `bin/configuration/add_saml_federations.py`.
+    saml_federation = SAMLFederation("Test federation", "http://incommon.org/metadata")
+    db.session.add(saml_federation)
+    db.session.commit()
+
+    # We shouldn't have any IdPs in any federation yet.
+    preexisting_idps: list[SAMLFederatedIdentityProvider] = db.session.query(
+        SAMLFederatedIdentityProvider
+    ).all()
+    assert not preexisting_idps
+
+    # Now we'll set up some mocking for the update.
+    idps_to_add = [
         SAMLFederatedIdentityProvider(
-            expected_federation,
+            saml_federation,
             saml_strings.IDP_1_ENTITY_ID,
             saml_strings.IDP_1_UI_INFO_EN_DISPLAY_NAME,
             saml_strings.CORRECT_XML_WITH_IDP_1,
         ),
         SAMLFederatedIdentityProvider(
-            expected_federation,
+            saml_federation,
             saml_strings.IDP_2_ENTITY_ID,
             saml_strings.IDP_2_UI_INFO_EN_DISPLAY_NAME,
             saml_strings.CORRECT_XML_WITH_IDP_2,
         ),
     ]
 
-    db.session.add_all([expected_federation])
-    db.session.add_all(expected_federated_identity_providers)
-
     loader = create_autospec(spec=SAMLFederatedIdentityProviderLoader)
-    loader.load = MagicMock(return_value=expected_federated_identity_providers)
+    loader.load = MagicMock(return_value=idps_to_add)
 
     with patch(
         "palace.manager.celery.tasks.saml._create_saml_federated_identity_provider_loader"
     ) as create_loader:
         create_loader.return_value = loader
 
-        # Invoke
+        # Run the actual update task.
         update_saml_federation_idps_metadata.delay().wait()
-        # Assert
-        identity_providers: list[SAMLFederatedIdentityProvider] = db.session.query(
-            SAMLFederatedIdentityProvider
-        ).all()
-        assert expected_federated_identity_providers == identity_providers
+
+    # The added IdPs should remain in the database after the task runs.
+    identity_providers: list[SAMLFederatedIdentityProvider] = db.session.query(
+        SAMLFederatedIdentityProvider
+    ).all()
+    assert len(identity_providers) == 2
+    assert idps_to_add == identity_providers
 
 
 def test_create_saml_federated_identity_provider_loader():
