1e569a2e73aaa704c7a6f531042f162adff6e4d3
Daniel Bernstein <dabylon@gmail.com>
2025-12-05 13:11:01 -0800

    Revert "[PP-3215] remove old overdrive scripts (#2889)"
    This reverts commit 598fcf4709c00a3e90e3197a8bd09796fb50d981.

--- src/palace/manager/service/celery/celery.py
+++ src/palace/manager/service/celery/celery.py
@@ -47,7 +47,6 @@ def beat_schedule() -> dict[str, Any]:
         opds2,
         opds_for_distributors,
         opds_odl,
-        overdrive,
         playtime_entries,
         reaper,
         rotate_jwe_key,
@@ -259,10 +258,6 @@ def beat_schedule() -> dict[str, Any]:
                 hour="0",
                 day_of_week="2",
             ),  # Every Tuesday at midnight
-        },
-        "overdrive_import_all_collections": {
-            "task": overdrive.import_all_collections.name,
-            "schedule": crontab(minute="*/15"),  # Run every 15 minutes
         },
     }
 
