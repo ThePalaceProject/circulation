062eb2e2b5ffc319452190d45a03d5a7f0fadbbd
Daniel Bernstein <dabylon@gmail.com>
2025-12-12 13:24:30 -0800

    [PP-3376] Makes library_uuids param optional for generating quicksight urls.  If not present, all allowable libraries are used to generate embed url.  This change is necessary to present excessively long urls from being passed to the CM.

--- src/palace/manager/api/admin/model/quicksight.py
+++ src/palace/manager/api/admin/model/quicksight.py
@@ -7,13 +7,16 @@ from palace.manager.util.flask_util import CustomBaseModel, str_comma_list_valid
 
 
 class QuicksightGenerateUrlRequest(CustomBaseModel):
-    library_uuids: list[UUID] = Field(
-        description="The list of libraries to include in the dataset, an empty list is equivalent to all the libraries the user is allowed to access."
+    library_uuids: list[UUID] | None = Field(
+        description="The list of libraries to include in the dataset, an empty list is equivalent to all the libraries the user is allowed to access.",
+        default=None,
     )
 
     @field_validator("library_uuids", mode="before")
     @classmethod
     def parse_library_uuids(cls, value: Any) -> list[str]:
+        if value is None:
+            return []
         return str_comma_list_validator(value)
 
 
