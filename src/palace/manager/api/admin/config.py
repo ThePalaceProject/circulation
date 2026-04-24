cf36b046d4f50af84e5ed90bc75d4e4c0a9af057
Jonathan Green <jonathan@razzard.com>
2026-04-24 09:49:30 -0300

    Bump Admin UI version to 1.40.0

--- src/palace/manager/api/admin/config.py
+++ src/palace/manager/api/admin/config.py
@@ -86,7 +86,7 @@ class OperationalMode(str, Enum):
 class Configuration(LoggerMixin):
     APP_NAME = "Palace Collection Manager"
     PACKAGE_NAME = "@thepalaceproject/circulation-admin"
-    PACKAGE_VERSION = "1.39.0"
+    PACKAGE_VERSION = "1.40.0"
 
     STATIC_ASSETS = {
         "admin_js": "circulation-admin.js",
